575d9a228c21e7e4b5d47e4a26c433ee7b57e34d
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-10-27 12:10:46 -0500

    [new] sui-bundler

---
+++ packages/bundler/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const execa = require( 'execa' )
+
+const babel = ( env, ...args ) => {
+	return execa( 'babel', [
+		'--verbose',
+		...args.filter( Boolean ),
+		'--root-mode=upward'
+	], {
+		stdio: 'inherit',
+		env: {
+			NODE_ENV: env
+		}
+	})
+}
+
+babel( null, 'src', '-d', 'dist' )
+babel( 'esm', 'src', '-o', 'dist/index.esm.js' )
