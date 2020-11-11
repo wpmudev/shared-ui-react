9cf8b2f14104e8931ff8ba182b2da4a4f8509c86
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-11-11 01:14:51 -0500

    ✨ Install storybook.
    Storybook will help us showcase and test our components.

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../packages/**/*.stories.js"
+  ],
+  "addons": [
+	"@storybook/addon-links",
+	"@storybook/addon-actions",
+    "@storybook/addon-essentials"
+  ]
+}
