f7138a45a6b6f99cc7110156754078e344f927ca
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-11-11 10:44:58 -0500

    🎨 [Button] Allow component classname variations.

--- packages/button/lib/button.js
+++ packages/button/lib/button.js
@@ -1,6 +1,25 @@
-import React from 'react'
-import styles from './styles.css'
+import React from 'react';
+import cx from 'clsx';
+import styles from './styles.css';
 
-const Button = ({ children }) => <button className={ styles.button }>{ children }</button>
+const Button = ({ children, className, color, ...rest }) => {
+	const classes = cx(
+		styles.Button,
+		{
+			[styles.ButtonPrimary]: color === '',
+			[styles.ButtonBlue]: color === 'blue'
+		},
+		className
+	);
+
+	return (
+		<button
+			{ ...rest }
+			className={ classes }
+		>
+			{ children }
+		</button>
+	);
+};
 
 export { Button };
