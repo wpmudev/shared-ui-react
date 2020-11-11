cff2214918d3d0989b6f1f6e44a4e1d6bd66d532
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-11-11 10:54:05 -0500

    🚑 Fix storybook setup.

--- .storybook/main.js
+++ .storybook/main.js
@@ -26,6 +26,13 @@ module.exports = {
 			],
 		});
 
+		// Change the order of resolution of main fields.
+		config.resolve.mainFields = [
+			'src',
+			'module',
+			'main'
+		];
+
 		// Return the altered config
 		return config;
 	},
