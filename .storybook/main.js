94c6e28b024a340b2cc60500fe6da8030082d2b7
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-11-11 10:13:55 -0500

    🔧 Storybook support for CSS modules.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,32 @@
 module.exports = {
-  "stories": [
-    "../packages/**/*.stories.js"
-  ],
-  "addons": [
-	"@storybook/addon-links",
-	"@storybook/addon-actions",
-    "@storybook/addon-essentials"
-  ]
+	stories: [
+		'../packages/**/*.stories.js'
+	],
+	addons: [
+		"@storybook/addon-actions",
+		"@storybook/addon-links"
+	],
+	webpackFinal: async ( config ) => {
+		// Remove default css rule from storybook.
+		config.module.rules = config.module.rules.filter(
+			( f ) => f.test.toString() !== '/\\.css$/'
+		);
+
+		// Push our custom easy one.
+		config.module.rules.push({
+			test: /\.css$/,
+			use: [
+				'style-loader',
+				{
+					loader: 'css-loader',
+					options: {
+						modules: true,
+					},
+				},
+			],
+		});
+
+		// Return the altered config
+		return config;
+	},
 }
