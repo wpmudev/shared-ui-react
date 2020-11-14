827edf28896518c5d7b1a166897c5bf5acf4b62f
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-11-14 10:39:49 -0500

    🎨 [Button] Renaming stories.

--- packages/button/docs/button.stories.js
+++ packages/button/docs/button.stories.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Button } from '../lib/button';
 
 export default {
-	title: 'Button'
+	title: 'Button',
+	component: Button,
 }
 
 export const primary = () => <Button label="Hello button" />
+primary.storyName = 'Default';
 
 export const secondary = () => <Button label="Hello button" ghost={ true } />
+secondary.storyName = 'Outlined';
