4d61b8e7096d159d78a9dddcb38a387475a12277
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-11-14 01:10:17 -0500

    🎨 Wrap properly all components for SUI styles to load correctly.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,16 @@
-// Get latest SUI styles.
-import '@wpmudev/shared-ui/dist/css/shared-ui.min.css'
+import React from 'react';
+import '@wpmudev/shared-ui/dist/css/shared-ui.min.css'; // Get latest SUI styles.
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+export const decorators = [
+	( Story ) => (
+		<div className="sui-2-10-0">
+			<div className="sui-wrap">
+				<Story />
+			</div>
+		</div>
+	),
+]
