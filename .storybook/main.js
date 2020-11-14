669e1132f6b2226e8e022b28cc94653e25186c99
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-11-14 00:53:15 -0500

    🔥 Remove default css rule.

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,25 +9,6 @@ module.exports = {
 		"@storybook/preset-scss"
 	],
 	webpackFinal: async ( config ) => {
-		// Remove default css rule from storybook.
-		config.module.rules = config.module.rules.filter(
-			( f ) => f.test.toString() !== '/\\.css$/'
-		);
-
-		// Push our custom easy one.
-		config.module.rules.push({
-			test: /\.css$/,
-			use: [
-				'style-loader',
-				{
-					loader: 'css-loader',
-					options: {
-						modules: true,
-					},
-				},
-			],
-		});
-
 		// Change the order of resolution of main fields.
 		config.resolve.mainFields = [
 			'src',
