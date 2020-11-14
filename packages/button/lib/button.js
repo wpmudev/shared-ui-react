4da8f66b1166cd1fac2da6b6f596ebbc204c7e7f
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-11-14 00:48:24 -0500

    🎨 [Button] Improve component structure.

--- packages/button/lib/button.js
+++ packages/button/lib/button.js
@@ -1,23 +1,51 @@
 import React from 'react';
-import cx from 'clsx';
-import styles from './styles.css';
 
-const Button = ({ children, className, color, ...rest }) => {
-	const classes = cx(
-		styles.Button,
-		{
-			[styles.ButtonPrimary]: color === '',
-			[styles.ButtonBlue]: color === 'blue'
-		},
-		className
-	);
+const Button = ({
+	label,
+	icon,
+	iconPosition,
+	size,
+	color,
+	ghost,
+	...props
+}) => {
+	let content = label;
+	let classes = 'sui-button';
+
+	if ( ghost ) {
+		classes += ' sui-button-ghost';
+	}
+
+	switch ( color ) {
+		case 'blue':
+		case 'green':
+		case 'red':
+		case 'orange':
+		case 'yellow':
+		case 'purple':
+			classes += ' sui-button-' + color;
+			break;
+	}
+
+	switch ( size ) {
+		case 'lg':
+		case 'large':
+			classes += ' sui-button-lg';
+			break;
+	}
+
+	if ( icon ) {
+		content = 'right' === iconPosition
+			? <React.Fragment>{ label } <span className={ 'sui-icon-' + icon } aria-hidden="true" /></React.Fragment>
+			: <React.Fragment><span className={ 'sui-icon-' + icon } aria-hidden="true" /> { label }</React.Fragment>
+	}
 
 	return (
 		<button
-			{ ...rest }
 			className={ classes }
+			{ ...props }
 		>
-			{ children }
+			{ content }
 		</button>
 	);
 };
