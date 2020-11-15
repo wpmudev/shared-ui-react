3f25db9a4b0388d1632ad190e9d0e972b1d7849c
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-11-15 12:43:32 -0500

    🔥 [Tutorials] Remove package.

--- packages/tutorials/__tests__/tutorials.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const tutorials = require('..');
-
-describe('tutorials', () => {
-    it('needs tests');
-});
--- packages/tutorials/lib/tutorials.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = tutorials;
-
-function tutorials() {
-    // TODO
-}
