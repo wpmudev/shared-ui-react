a035b7160f759b2cde65cab1fd462bfaecc3017b
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-11-19 00:59:12 -0500

    🎨 [Tutorials] Load component on Storybook.

---
+++ packages/tutorials/docs/tutorials.stories.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Tutorials } from '../lib/tutorials';
+
+export default {
+	title: 'Tutorials'
+}
+
+export const primary = () =>
+	<Tutorials category='11234' />
