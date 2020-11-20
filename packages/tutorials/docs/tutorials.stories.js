0431527ad61e5b68d9e7fb42410fb787b46855c1
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-11-20 01:50:57 -0500

    🔧 [Tutorials] Show controls with category data.

--- packages/tutorials/docs/tutorials.stories.js
+++ packages/tutorials/docs/tutorials.stories.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import { Tutorials } from '../lib/tutorials';
 
+const plugins = {
+	smush: '11228',
+	hustle: '11235',
+	defender: '11231',
+	forminator: '11232',
+	smartcrawl: '11238',
+	hummingbird: '11234'
+};
+
 export default {
-	title: 'Tutorials'
+	title: 'Tutorials',
+	component: Tutorials,
+	argTypes: {
+		category: {
+			type: {
+				required: true
+			},
+			control: {
+				type: 'select',
+				default: '11231',
+				options: plugins,
+			},
+		}
+	}
 }
 
-const smush = '11228';
-const hustle = '11235';
 const defender = '11231';
-const forminator = '11232';
-const smartcrawl = '11238';
-const hummingbird = '11234';
 
-export const primary = () => <Tutorials category={ defender } />
+const Template = ( args ) => <Tutorials { ...args } />
+
+export const primary = Template.bind({});
+primary.storyName = 'Page';
+primary.args = {
+	title: 'Tutorials',
+	category: defender
+};
+
+export const secondary = Template.bind({});
+secondary.storyName = 'Widget';
+secondary.args = {
+	category: defender
+};
