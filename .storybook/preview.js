60a9d8019d9094b509f2b6378d01c1271a7dc4a7
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-11-20 00:49:01 -0500

    🔧 [Storybook] Add custom viewports.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,60 @@
 import React from 'react';
 import '@wpmudev/shared-ui/dist/css/shared-ui.min.css'; // Get latest SUI styles.
 
+const customViewports = {
+	xl: {
+		name: 'Larger',
+		styles: {
+			width: '1500px',
+			height: '938px'
+		},
+		type: 'desktop'
+	},
+	lg: {
+		name: 'Large',
+		styles: {
+			width: '1200px',
+			height: '750px'
+		},
+		type: 'desktop'
+	},
+	md: {
+		name: 'Medium',
+		styles: {
+			width: '783px',
+			height: '1253px'
+		},
+		type: 'tablet'
+	},
+	sm: {
+		name: 'Small',
+		styles: {
+			width: '480px',
+			height: '800px'
+		},
+		type: 'mobile'
+	},
+	xs: {
+		name: 'Smaller',
+		styles: {
+			width: '375px',
+			height: '667px'
+		},
+		type: 'mobile'
+	},
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   backgrounds: {
-	  default: 'wordpress',
+	  default: 'WordPress',
 	  values: [
-		  { name: 'wordpress', value: '#f1f1f1' }
+		  { name: 'WordPress', value: '#f1f1f1' }
 	  ]
-  }
+  },
+  viewport: {
+	  viewports: customViewports
+  },
 }
 
 export const decorators = [
