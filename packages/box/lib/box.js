362170e44731d4307c0ab8ed1164a308bc71330e
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-11-28 02:25:04 -0500

    🎨 [Box] Create box wrapper, header, body and footer components.

--- packages/box/lib/box.js
+++ packages/box/lib/box.js
@@ -1,7 +1,98 @@
-'use strict';
+import React from 'react';
 
-module.exports = box;
+export const Box = ({
+	children,
+	...props
+}) => {
+	return (
+		<div
+			className="sui-box"
+			{ ...props }
+		>
+			{ children }
+		</div>
+	);
+}
 
-function box() {
-    // TODO
+export const BoxHeader = ({
+	title,
+	titleIcon = '',
+	titleTag = '',
+	titleTagColor = '',
+	children,
+	...props
+}) => {
+	let titleTagClass = 'sui-tag';
+
+	switch( titleTagColor ) {
+		case 'red':
+		case 'yellow':
+		case 'green':
+		case 'blue':
+		case 'purple':
+			titleTagClass += ' sui-tag-' + titleTagColor;
+			break;
+	}
+
+	return (
+		<div
+			className="sui-box-header"
+			{ ...props }
+		>
+			{ '' !== title &&
+				<h3 className="sui-box-title">
+					{ '' !== titleIcon &&
+						<span
+							className={ `sui-icon-${ titleIcon } sui-md` }
+							aria-hidden="true"
+						/>
+					}
+					{ title }
+					{ '' !== titleTag &&
+						<span
+							className={ titleTagClass }
+							style={ {
+								verticalAlign: 'top',
+								marginTop: 2,
+								marginLeft: 10,
+							} }
+						>{ titleTag }</span>
+					}
+				</h3>
+			}
+
+			<div className="sui-actions-right">
+				{ children }
+			</div>
+
+		</div>
+	);
+}
+
+export const BoxBody = ({
+	children,
+	...props
+}) => {
+	return (
+		<div
+			className="sui-box-body"
+			{ ...props }
+		>
+			{ children }
+		</div>
+	);
+}
+
+export const BoxFooter = ({
+	children,
+	...props
+}) => {
+	return (
+		<div
+			className="sui-box-footer"
+			{ ...props }
+		>
+			{ children }
+		</div>
+	);
 }
