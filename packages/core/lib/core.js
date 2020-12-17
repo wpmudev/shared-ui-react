80d84ae123bf6144269f11e18691a0a77f11c8b7
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-12-17 12:27:12 -0500

    ➕ [Core] Include packages to core.

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,10 +1,21 @@
-import { Box, BoxHeader, BoxBody, BoxFooter } from '@wpmudev/react-button'
+import {
+	Box,
+	BoxHeader,
+	BoxTitle,
+	BoxBody,
+	BoxFooter
+} from '@wpmudev/react-box'
 import { Button } from '@wpmudev/react-button'
+import { Post } from '@wpmudev/react-post'
+import { Notifications } from '@wpmudev/react-notifications'
 
 export {
 	Box,
 	BoxHeader,
+	BoxTitle,
 	BoxBody,
 	BoxFooter,
-	Button
+	Button,
+	Post,
+	Notifications
 }
