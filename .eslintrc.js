48840f04fc3180916e2b944543f70329770c73d0
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-01-26 09:00:48 -0500

    🐛 fix/eslint: Improve prettier configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,45 @@
+/* eslint-disable no-undef */
 module.exports = {
-    env: {
+	env: {
 		browser: true,
 		es2021: true
 	},
 	extends: [
-        "eslint:recommended", // For catching common problems in JavaScript.
-		"plugin:react/recommended", // For fixing a lot of React problems.
-		"plugin:react-hooks/recommended", // For fixing problems with React hooks.
-		"plugin:jsx-a11y/recommended", // For fixing accessibility problems for HTML.
-		"plugin:prettier/recommended", // For formatting our code with common rules.
-		"prettier/react" // For telling Prettier that we are using React.
+		'eslint:recommended', // For catching common problems in JavaScript.
+		'plugin:react/recommended', // For fixing a lot of React problems.
+		'plugin:react-hooks/recommended', // For fixing problems with React hooks.
+		'plugin:jsx-a11y/recommended', // For fixing accessibility problems for HTML.
+		'plugin:prettier/recommended', // For formatting our code with common rules.
+		'prettier/react' // For telling Prettier that we are using React.
 	],
 	parserOptions: {
-        ecmaFeatures: {
-            jsx: true // Enable jsx for our parser.
-        },
-        ecmaVersion: 2021, // Last version of ECMAScript syntax.
-        sourceType: "module" // Use import & export.
+		ecmaFeatures: {
+			jsx: true // Enable jsx for our parser.
+		},
+		ecmaVersion: 2021, // Last version of ECMAScript syntax.
+		sourceType: 'module' // Use import & export.
 	},
-	parser: "@babel/eslint-parser",
+	parser: '@babel/eslint-parser',
 	settings: {
 		react: {
-			version: "detect" // Auto-detect the React version.
+			version: 'detect' // Auto-detect the React version.
 		}
 	},
-    plugins: [
-        "react"
-    ],
-    rules: {
-		"prettier/prettier": [
-			"error",
+	plugins: ['react'],
+	rules: {
+		'prettier/prettier': [
+			'error',
 			{
-				semi: false,
-				singleQuote: true,
 				printWidth: 100,
-				tabWidth: 4,
-				jsxSingleQuote: false,
-				trailingComma: "none",
-				arrowParens: "avoid"
+				useTabs: true,
+				semi: true,
+				singleQuote: true,
+				quoteProps: 'as-needed',
+				jsxSingleQuote: true,
+				trailingComma: 'none',
+				bracketSpacing: true,
+				jsxBracketSameLine: true,
+				arrowParens: 'avoid'
 			}
 		]
 	}
