c6132046989229e985d766b1cd1364ac232bb481
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-03-17 19:52:45 -0500

    🐛 new/button-icon: Improve component.

--- packages/react-button-icon/lib/react-button-icon.js
+++ packages/react-button-icon/lib/react-button-icon.js
@@ -1,33 +1,78 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class ButtonIcon extends Component {
-	constructor(props) {
-		super(props);
+const ButtonIcon = ({ label, icon, design = "solid", color, ...props }) => {
+	const loader = (
+		<span
+			className="sui-icon-loader sui-loading"
+			style={{ position: "relative" }}
+			aria-hidden="true"
+		/>
+	);
+
+	let content = (
+		<React.Fragment>
+			<span className={"sui-icon-" + icon} aria-hidden="true" />
+			<span className="sui-screen-reader-text">{label}</span>
+		</React.Fragment>
+	);
+
+	let className = "sui-button-icon";
+
+	// Set button color.
+	switch (color) {
+		case "blue":
+		case "green":
+		case "red":
+		case "orange":
+		case "purple":
+		case "yellow":
+		case "white":
+			className += " sui-button-" + color;
+			break;
+
+		case "gray":
+		default:
+			className += "";
+			break;
 	}
 
-	render() {
-		if (this.props.href && "" !== this.props.href) {
-			return (
-				<a className="sui-button-icon" {...this.props}>
-					<span className={`sui-icon-${this.props.icon}`} aria-hidden="true" />
-					<span className="sui-screen-reader-text">
-						{this.props.label && "" !== this.props.label
-							? this.props.label
-							: "Button"}
-					</span>
-				</a>
-			);
-		}
+	// Set button style.
+	switch (design) {
+		case "ghost":
+		case "outlined":
+			className += " sui-button-" + design;
+			break;
 
+		case "solid":
+		default:
+			className += "";
+			break;
+	}
+
+	// Set loading class.
+	if (props.loading) {
+		className += " sui-button-onload";
+	}
+
+	if (props.href) {
 		return (
-			<button className="sui-button-icon" {...this.props}>
-				<span className={`sui-icon-${this.props.icon}`} aria-hidden="true" />
-				<span className="sui-screen-reader-text">
-					{this.props.label && "" !== this.props.label
-						? this.props.label
-						: "Button"}
-				</span>
-			</button>
+			<a
+				className={className}
+				disabled={props.disabled || props.loading}
+				{...props}>
+				{props.loading ? loader : content}
+			</a>
 		);
 	}
-}
+
+	return (
+		<button
+			className={className}
+			disabled={props.disabled || props.loading}
+			{...props}>
+			{props.loading ? loader : content}
+		</button>
+	);
+};
+
+export { ButtonIcon };
