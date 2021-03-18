37e3f5c0317774c4db52f711e900e3bd2b695142
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-03-18 14:49:06 -0500

    ✨ new(react-input): Add component markup.

--- packages/react-input/lib/react-input.js
+++ packages/react-input/lib/react-input.js
@@ -1,7 +1,50 @@
-'use strict';
+import React, { Component } from 'react';
 
-module.exports = reactInput;
+export class Input extends Component {
+    constructor( props ) {
+        super( props );
+    }
 
-function reactInput() {
-    // TODO
+    render() {
+        const uniqueId = ( this.props.id && '' !== this.props.id )
+			? this.props.id
+			: this.props.property;
+
+		const type = ( this.props.type && '' !== this.props.type )
+			? this.props.type
+			: 'text';
+
+		let clazz = 'sui-form-control';
+
+        switch ( this.props.size ) {
+            case 'sm':
+            case 'small':
+                clazz += ' sui-input-sm';
+                break;
+
+            case 'md':
+            case 'medium':
+                clazz += ' sui-input-md';
+                break;
+        }
+
+        return (
+            <div className="sui-form-field">
+                <label for={ uniqueId } className="sui-label">{ this.props.label }</label>
+                <input
+                    id={ uniqueId }
+                    type={ type }
+                    { ... ( this.props.min && 'number' === type ) && { min: this.props.min } }
+                    { ... ( this.props.max && 'number' === type ) && { max: this.props.max } }
+                    { ... ( this.props.maxLength && 'text' === type ) && { maxlength: this.props.maxLength } }
+                    { ... ( this.props.placeholder && '' !== this.props.placeholder ) && { placeholder: this.props.placeholder } }
+                    className={ clazz }
+                    { ... this.props.disabled && { disabled: true } }
+                />
+                { this.props.description &&
+                    <p className="sui-description">{ this.props.description }</p>
+                }
+            </div>
+        );
+    }
 }
