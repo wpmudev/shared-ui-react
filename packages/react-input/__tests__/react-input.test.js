c40e5e1c6084295067f1f1cc79714533bebd2cbe
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-03-18 11:08:07 -0500

    Revert "✨ new(react-input): Create component."
    This reverts commit 6ef4bda959b7fea2ae237c359df24b9dab1707f9.

--- packages/react-input/__tests__/react-input.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const reactInput = require('..');
-
-describe('@wpmudev/react-input', () => {
-    it('needs tests');
-});
--- packages/react-input/lib/react-input.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = reactInput;
-
-function reactInput() {
-    // TODO
-}
