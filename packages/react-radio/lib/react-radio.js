e09b36328e75523ae6311da91969a1d849b68945
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-03-22 12:43:32 -0500

    🎨 fix(react-radio): Show image in option.

--- packages/react-radio/lib/react-radio.js
+++ packages/react-radio/lib/react-radio.js
@@ -1,4 +1,4 @@
-import React, { Component, Children } from 'react';
+import React, { Component, Children, Fragment } from 'react';
 
 export class Radio extends Component {
     constructor( props ) {
@@ -7,17 +7,69 @@ export class Radio extends Component {
 
     render() {
         const Options = Children.map( this.props.children, option => {
-            return (
-                <label htmlFor={ `${ option.props.value }-option` } className="sui-radio">
+            const hasImage1x = option.props.image && '' !== option.props.image;
+            const hasImage2x = option.props.image2x && '' !== option.props.image2x;
+
+            let image1x = '';
+            let image2x = '';
+            let imageHtml = '';
+
+            if ( hasImage1x ) {
+                image1x = option.props.image;
+            }
+
+            if ( hasImage2x ) {
+                image2x = option.props.image2x;
+                imageHtml = (
+                    <img
+                        src={ image1x }
+                        srcSet={ `${ image1x } 1x, ${ image2x } 2x` }
+                        alt=""
+                        aria-hidden="true"
+                    />
+                );
+            } else {
+                imageHtml = <img src={ image1x } alt="" aria-hidden="true" />;
+            }
+
+            const input = (
+                <Fragment>
                     <input
                         type="radio"
+                        name={ this.props.property }
                         value={ option.props.value }
-                        id={ `${ option.props.value }-option` }
+                        id={ `${ option.props.value }--option` }
+                        aria-labelledby={ `${ option.props.value }--option-label` }
                     />
                     <span aria-hidden="true" />
                     { option.props.label &&
-                        <span>{ option.props.label }</span>
+                        <span id={ `${ option.props.value }--option-label` }>{ option.props.label }</span>
                     }
+                </Fragment>
+            );
+
+            // Option with image.
+            if ( hasImage1x ) {
+                return (
+                    <label
+                        htmlFor={ `${ option.props.value }-option` }
+                        className="sui-radio-image"
+                    >
+                        { hasImage1x && imageHtml }
+                        <span className="sui-radio">
+                            { input }
+                        </span>
+                    </label>
+                );
+            }
+            
+            // Option with text.
+            return (
+                <label
+                    htmlFor={ `${ option.props.value }-option` }
+                    className="sui-radio"
+                >
+                    { input }
                 </label>
             );
         });
