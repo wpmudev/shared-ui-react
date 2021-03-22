0f37650a9826d81234bbd2755db40b4ecb45dcd1
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-03-22 08:51:34 -0500

    🎨 new(react-radio): Add basic markup to component.

--- packages/react-radio/lib/react-radio.js
+++ packages/react-radio/lib/react-radio.js
@@ -1,7 +1,29 @@
-'use strict';
+import React, { Component, Children } from 'react';
 
-module.exports = reactRadio;
+export class Radio extends Component {
+    constructor( props ) {
+        super( props );
+    }
 
-function reactRadio() {
-    // TODO
+    render() {
+        const Options = Children.map( this.props.children, option => {
+            return (
+                <label htmlFor={ `${ option.props.value }-option` } className="sui-radio">
+                    <input
+                        type="radio"
+                        value={ option.props.value }
+                        id={ `${ option.props.value }-option` }
+                    />
+                    <span aria-hidden="true" />
+                    { option.props.label &&
+                        <span>{ option.props.label }</span>
+                    }
+                </label>
+            );
+        });
+
+        return (
+            <div role="radiogroup" className="sui-form-field">{ Options }</div>
+        );
+    }
 }
