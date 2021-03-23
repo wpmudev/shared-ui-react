4ece4e271fc5131bb231b4f57a11cfa91e884da7
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-03-23 17:24:19 -0500

    🎨 new(react-radio): Show with and without image variations.

--- packages/react-radio/docs/react-radio.stories.js
+++ packages/react-radio/docs/react-radio.stories.js
@@ -6,21 +6,90 @@ export default {
     component: Radio
 }
 
-const Template = args => <Radio {...args} />;
+const Template = ({ options, ...args }) => {
+    return (
+        <Radio { ...args }>
+            { options.map( ( option ) => (
+                <div { ...option } />
+            ) ) }
+        </Radio>
+    );
+};
 
 export const text = Template.bind({});
-text.storyName = 'Text Only';
+text.storyName = 'Text Option';
 text.args = {
-    property: 'options',
-    children: (
-        <div
-            label=""
-            value=""
-            { ...Option.args }
-        />
-    )
+    property: 'hot-beverages',
+    options: [
+        {
+            label: 'Espresso',
+            value: 'espresso'
+        },
+        {
+            label: 'Moccacino',
+            value: 'moccacino'
+        },
+        {
+            label: 'Capuccino',
+            value: 'capuccino'
+        }
+    ]
 };
-Option.args = {
-    label: 'Sample option',
-    value: 'sample-option'
+text.argTypes = {
+    size: {
+        control: {
+            type: 'select',
+            options: [
+                'default',
+                'small'
+            ]
+        }
+    },
+    alignment: {
+        control: {
+            type: 'select',
+            options: [
+                'inline',
+                'stacked'
+            ]
+        }
+    }
+};
+
+export const image = Template.bind({});
+image.storyName = 'Image Option';
+image.args = {
+    size: 'small',
+    property: 'wpmudev-plugins',
+    options: [
+        {
+            label: 'SmartCrawl',
+            value: 'smartcrawl',
+            image: 'https://wpmudev.github.io/shared-ui/assets/images/plugins-smartcrawl-icon.png',
+            image2x: 'https://wpmudev.github.io/shared-ui/assets/images/plugins-smartcrawl-icon@2x.png'
+        },
+        {
+            label: 'Defender',
+            value: 'defender',
+            image: 'https://wpmudev.github.io/shared-ui/assets/images/plugins-defender-icon.png',
+            image2x: 'https://wpmudev.github.io/shared-ui/assets/images/plugins-defender-icon@2x.png'
+        },
+        {
+            label: 'Smush',
+            value: 'smush',
+            image: 'https://wpmudev.github.io/shared-ui/assets/images/plugins-smush-icon.png',
+            image2x: 'https://wpmudev.github.io/shared-ui/assets/images/plugins-smush-icon@2x.png'
+        }
+    ]
+};
+image.argTypes = {
+    size: {
+        control: {
+            type: 'select',
+            options: [
+                'default',
+                'small'
+            ]
+        }
+    },
 };
