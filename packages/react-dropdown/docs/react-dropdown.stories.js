26bac30d45fcc4f37f5fdbb4f9cc40a1c0a2f443
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-03-26 21:40:21 -0500

    ✨ new(react-dropdown): Add stories samples.

---
+++ packages/react-dropdown/docs/react-dropdown.stories.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Dropdown } from '../lib/react-dropdown';
+
+export default {
+    title: 'Components/Dropdown'
+}
+
+const Template = ({ children, ...args }) => {
+    return (
+        <Dropdown { ...args }>
+            { children.map( ( child ) => (
+                <div { ...child } />
+            ) ) }
+        </Dropdown>
+    );
+};
+
+export const primary = Template.bind({});
+primary.storyName = 'Default';
+primary.args = {
+    position: 'left',
+    children: [
+        {
+            name: 'Option 1'
+        },
+        {
+            name: 'Option 2'
+        },
+        {
+            name: 'Option 3'
+        },
+    ]
+};
+primary.argTypes = {
+    position: {
+        control: {
+            type: 'select',
+            options: [
+                'left',
+                'center',
+                'right'
+            ]
+        }
+    }
+};
+
+export const secondary = Template.bind({});
+secondary.storyName = 'Option Icon';
+secondary.args = {
+    position: 'left',
+    children: [
+        {
+            name: 'Option 1',
+            icon: 'check'
+        },
+        {
+            name: 'Option 2',
+            icon: 'close'
+        },
+        {
+            name: 'Option 3',
+            icon: 'pencil'
+        },
+    ]
+};
+secondary.argTypes = {
+    ...primary.argTypes
+};
+
+export const tertiary = Template.bind({});
+tertiary.storyName = 'Option Tag';
+tertiary.args = {
+    position: 'left',
+    children: [
+        {
+            name: 'Option 1',
+            tag: 'Coming Soon'
+        },
+        {
+            name: 'Option 2',
+            tag: 'Coming Soon'
+        },
+        {
+            name: 'Option 3',
+            tag: 'Coming Soon'
+        },
+    ]
+};
+tertiary.argTypes = {
+    ...primary.argTypes
+};
