53457ce552f6029d9c2eb82c6575dce7d9879e8f
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-03-31 11:54:22 -0500

    🐛 fix(react-notifications): Add samples to storybook.

--- packages/react-notifications/docs/react-notifications.stories.js
+++ packages/react-notifications/docs/react-notifications.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Notifications } from '../lib/react-notifications';
 
 export default {
-    title: 'Components/Notifications'
+    title: 'Components/Notifications/Single-Line'
 }
 
 const TypeOptions = {
@@ -16,14 +16,16 @@ const TypeOptions = {
 
 const Template = ({ ...props }) => {
     return (
-        <Notifications {...props}></Notifications>
+        <Notifications {...props} />
     );
 }
 
 export const primary = Template.bind({});
 primary.storyName = 'Default';
 primary.args = {
-    message: 'Nulla vitae elit libero, a pharetra augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec ullamcorper nulla non metus auctor fringilla. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+    children: (
+        <p>Nulla vitae elit libero, a pharetra augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec ullamcorper nulla non metus auctor fringilla. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+    )
 };
 primary.argTypes = {
     type: {
@@ -32,4 +34,14 @@ primary.argTypes = {
             options: TypeOptions
         }
     }
+};
+
+export const secondary = Template.bind({});
+secondary.storyName = 'Dismissable';
+secondary.args = {
+    ...primary.args,
+    dismiss: true
+};
+secondary.argTypes = {
+    ...primary.argTypes
 };
