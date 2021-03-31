4ad41c60e19ab7b052b454064a755241b331fef7
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-03-30 20:39:41 -0500

    📦 build(react-button): Compile changes.

---
+++ packages/react-button/dist/react-button.esm.js
@@ -0,0 +1,127 @@
+import React from 'react';
+
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
+
+function _objectWithoutPropertiesLoose(source, excluded) {
+  if (source == null) return {};
+  var target = {};
+  var sourceKeys = Object.keys(source);
+  var key, i;
+
+  for (i = 0; i < sourceKeys.length; i++) {
+    key = sourceKeys[i];
+    if (excluded.indexOf(key) >= 0) continue;
+    target[key] = source[key];
+  }
+
+  return target;
+}
+
+function _objectWithoutProperties(source, excluded) {
+  if (source == null) return {};
+
+  var target = _objectWithoutPropertiesLoose(source, excluded);
+
+  var key, i;
+
+  if (Object.getOwnPropertySymbols) {
+    var sourceSymbolKeys = Object.getOwnPropertySymbols(source);
+
+    for (i = 0; i < sourceSymbolKeys.length; i++) {
+      key = sourceSymbolKeys[i];
+      if (excluded.indexOf(key) >= 0) continue;
+      if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue;
+      target[key] = source[key];
+    }
+  }
+
+  return target;
+}
+
+var Button = /*#__PURE__*/React.forwardRef(function (_ref) {
+  var label = _ref.label,
+      icon = _ref.icon,
+      _ref$design = _ref.design,
+      design = _ref$design === void 0 ? "solid" : _ref$design,
+      color = _ref.color,
+      className = _ref.className,
+      props = _objectWithoutProperties(_ref, ["label", "icon", "design", "color", "className"]);
+
+  var loader = /*#__PURE__*/React.createElement("span", {
+    className: "sui-icon-loader sui-loading",
+    style: {
+      position: "relative"
+    },
+    "aria-hidden": "true"
+  });
+  var content = /*#__PURE__*/React.createElement(React.Fragment, null, icon && "" !== icon && /*#__PURE__*/React.createElement("span", {
+    className: "sui-icon-" + icon,
+    "aria-hidden": "true"
+  }), label);
+  className = '' !== className ? 'sui-button ' + className : 'sui-button'; // Set button color.
+
+  switch (color) {
+    case "blue":
+    case "green":
+    case "red":
+    case "orange":
+    case "purple":
+    case "yellow":
+    case "white":
+      className += " sui-button-" + color;
+      break;
+
+    case "gray":
+    default:
+      className += "";
+      break;
+  } // Set button style.
+
+
+  switch (design) {
+    case "ghost":
+    case "outlined":
+      className += " sui-button-" + design;
+      break;
+
+    case "solid":
+    default:
+      className += "";
+      break;
+  } // Set loading class.
+
+
+  if (props.loading) {
+    className += " sui-button-onload";
+  }
+
+  if (props.href) {
+    return /*#__PURE__*/React.createElement("a", _extends({
+      className: className,
+      disabled: props.disabled || props.loading
+    }, props), props.loading ? loader : content);
+  }
+
+  return /*#__PURE__*/React.createElement("button", _extends({
+    className: className,
+    disabled: props.disabled || props.loading
+  }, props), props.loading ? loader : content);
+});
+
+export { Button };
