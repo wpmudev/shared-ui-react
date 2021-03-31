464f272a519b8208ceaa33eab609ba2cd58284d1
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-03-31 11:54:06 -0500

    🐛 fix(react-notifications): Make component dismissible.

--- packages/react-notifications/lib/react-notifications.js
+++ packages/react-notifications/lib/react-notifications.js
@@ -1,11 +1,26 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { ButtonIcon } from '@wpmudev/react-button-icon';
 
 export class Notifications extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			hide: false
+		};
+
+		this.close = this.close.bind( this );
+	}
+
+	close = () => {
+		this.setState({
+			hide: true
+		});
 	}
 
 	render() {
+		const { hide } = this.state;
+		
 		let classMain = "sui-notice";
 		let classIcon = "sui-notice-icon sui-md";
 
@@ -28,16 +43,34 @@ export class Notifications extends Component {
 				break;
 		}
 
-		return (
-			<div className={classMain}>
-				<div className="sui-notice-content">
-					<div className="sui-notice-message">
-						<span className={classIcon} aria-hidden="true" />
-
-						<p>{this.props.message}</p>
-					</div>
-				</div>
+		const message = (
+			<div className="sui-notice-message">
+				<span className={classIcon} aria-hidden="true" />
+				{this.props.children}
 			</div>
 		);
+
+		const actions = (
+			<div className="sui-notice-actions">
+				<ButtonIcon
+					icon="check"
+					label="Hide"
+					onClick={ e => this.close(e) }
+				/>
+			</div>
+		);
+
+		if ( !hide ) {
+			return (
+				<div className={classMain}>
+					<div className="sui-notice-content">
+						{ message }
+						{ this.props.dismiss && actions }
+					</div>
+				</div>
+			);
+		}
+
+		return null;
 	}
 }
