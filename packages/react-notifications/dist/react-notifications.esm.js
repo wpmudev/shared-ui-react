cb46102ee160fdb0784fad80cb6ed6e3e349ee98
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-04-07 20:56:53 -0500

    📦 build(react-notifications): Compile changes.

--- packages/react-notifications/dist/react-notifications.esm.js
+++ packages/react-notifications/dist/react-notifications.esm.js
@@ -22,6 +22,21 @@ function _createClass(Constructor, protoProps, staticProps) {
   return Constructor;
 }
 
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
 function _inherits(subClass, superClass) {
   if (typeof superClass !== "function" && superClass !== null) {
     throw new TypeError("Super expression must either be null or a function");
@@ -101,20 +116,165 @@ function _createSuper(Derived) {
   };
 }
 
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
+
+function _objectWithoutPropertiesLoose(source, excluded) {
+  if (source == null) return {};
+  var target = {};
+  var sourceKeys = Object.keys(source);
+  var key, i;
+
+  for (i = 0; i < sourceKeys.length; i++) {
+    key = sourceKeys[i];
+    if (excluded.indexOf(key) >= 0) continue;
+    target[key] = source[key];
+  }
+
+  return target;
+}
+
+function _objectWithoutProperties(source, excluded) {
+  if (source == null) return {};
+
+  var target = _objectWithoutPropertiesLoose(source, excluded);
+
+  var key, i;
+
+  if (Object.getOwnPropertySymbols) {
+    var sourceSymbolKeys = Object.getOwnPropertySymbols(source);
+
+    for (i = 0; i < sourceSymbolKeys.length; i++) {
+      key = sourceSymbolKeys[i];
+      if (excluded.indexOf(key) >= 0) continue;
+      if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue;
+      target[key] = source[key];
+    }
+  }
+
+  return target;
+}
+
+var ButtonIcon = /*#__PURE__*/React.forwardRef(function (_ref, ref) {
+  var label = _ref.label,
+      icon = _ref.icon,
+      _ref$design = _ref.design,
+      design = _ref$design === void 0 ? "solid" : _ref$design,
+      color = _ref.color,
+      className = _ref.className,
+      props = _objectWithoutProperties(_ref, ["label", "icon", "design", "color", "className"]);
+
+  var loader = /*#__PURE__*/React.createElement("span", {
+    className: "sui-icon-loader sui-loading",
+    style: {
+      position: "relative"
+    },
+    "aria-hidden": "true"
+  });
+  var content = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("span", {
+    className: "sui-icon-" + icon,
+    "aria-hidden": "true"
+  }), /*#__PURE__*/React.createElement("span", {
+    className: "sui-screen-reader-text"
+  }, label));
+  className = '' !== className ? 'sui-button-icon ' + className : 'sui-button-icon'; // Set button color.
+
+  switch (color) {
+    case "blue":
+    case "green":
+    case "red":
+    case "orange":
+    case "purple":
+    case "yellow":
+    case "white":
+      className += " sui-button-" + color;
+      break;
+
+    case "gray":
+    default:
+      className += "";
+      break;
+  } // Set button style.
+
+
+  switch (design) {
+    case "ghost":
+    case "outlined":
+      className += " sui-button-" + design;
+      break;
+
+    case "solid":
+    default:
+      className += "";
+      break;
+  } // Set loading class.
+
+
+  if (props.loading) {
+    className += " sui-button-onload";
+  }
+
+  if (props.href) {
+    return /*#__PURE__*/React.createElement("a", _extends({
+      ref: ref,
+      className: className,
+      disabled: props.disabled || props.loading
+    }, props), props.loading ? loader : content);
+  }
+
+  return /*#__PURE__*/React.createElement("button", _extends({
+    ref: ref,
+    className: className,
+    disabled: props.disabled || props.loading
+  }, props), props.loading ? loader : content);
+});
+
 var Notifications = /*#__PURE__*/function (_Component) {
   _inherits(Notifications, _Component);
 
   var _super = _createSuper(Notifications);
 
   function Notifications(props) {
+    var _this;
+
     _classCallCheck(this, Notifications);
 
-    return _super.call(this, props);
+    _this = _super.call(this, props);
+
+    _defineProperty(_assertThisInitialized(_this), "close", function () {
+      _this.setState({
+        hide: true
+      });
+    });
+
+    _this.state = {
+      hide: false
+    };
+    _this.close = _this.close.bind(_assertThisInitialized(_this));
+    return _this;
   }
 
   _createClass(Notifications, [{
     key: "render",
     value: function render() {
+      var _this2 = this;
+
+      var hide = this.state.hide;
       var classMain = "sui-notice";
       var classIcon = "sui-notice-icon sui-md";
 
@@ -125,6 +285,7 @@ var Notifications = /*#__PURE__*/function (_Component) {
         case "error":
         case "upsell":
           classMain += " sui-notice-" + this.props.type;
+          classIcon += " sui-icon-info";
           break;
 
         case "loading":
@@ -136,16 +297,31 @@ var Notifications = /*#__PURE__*/function (_Component) {
           break;
       }
 
-      return /*#__PURE__*/React.createElement("div", {
-        className: classMain
-      }, /*#__PURE__*/React.createElement("div", {
-        className: "sui-notice-content"
-      }, /*#__PURE__*/React.createElement("div", {
+      var message = /*#__PURE__*/React.createElement("div", {
         className: "sui-notice-message"
       }, /*#__PURE__*/React.createElement("span", {
         className: classIcon,
         "aria-hidden": "true"
-      }), /*#__PURE__*/React.createElement("p", null, this.props.message))));
+      }), this.props.children);
+      var actions = /*#__PURE__*/React.createElement("div", {
+        className: "sui-notice-actions"
+      }, /*#__PURE__*/React.createElement(ButtonIcon, {
+        icon: "check",
+        label: "Hide",
+        onClick: function onClick(e) {
+          return _this2.close(e);
+        }
+      }));
+
+      if (!hide) {
+        return /*#__PURE__*/React.createElement("div", {
+          className: classMain
+        }, /*#__PURE__*/React.createElement("div", {
+          className: "sui-notice-content"
+        }, message, this.props.dismiss && actions));
+      }
+
+      return null;
     }
   }]);
 
