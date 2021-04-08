d53d076a7935ab8a6007d0f0b1955c7079a88a17
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-04-08 16:45:50 -0500

    📝 docs(react-box): Add new footer args to stories.

--- packages/react-box/docs/box-footer.stories.js
+++ packages/react-box/docs/box-footer.stories.js
@@ -22,6 +22,112 @@ export const SimpleFooter = Template.bind({});
 SimpleFooter.storyName = 'Single Action';
 SimpleFooter.args = {};
 SimpleFooter.argTypes = {
+    display: {
+        name: 'Display',
+        type: {
+            name: 'string',
+            required: false
+        },
+        description: 'Description goes here...',
+        control: {
+            type: 'select',
+            options: {
+                block: 'block',
+                inline: 'inline'
+            }
+        },
+    },
+    alignment: {
+        name: 'Alignment',
+        type: {
+            name: 'string',
+            required: false
+        },
+        description: 'Description goes here...',
+        control: {
+            type: 'select',
+            options: {
+                default: '',
+                center: 'center'
+            }
+        },
+    },
+    paddingTop: {
+        name: 'Padding Top',
+        type: {
+            name: 'string',
+            required: false
+        },
+        description: 'Description goes here...',
+        defaultValue: 30,
+        control: {
+            type: 'number',
+            min: 0
+        },
+    },
+    paddingRight: {
+        name: 'Padding Right',
+        type: {
+            name: 'string',
+            required: false
+        },
+        description: 'Description goes here...',
+        defaultValue: 30,
+        control: {
+            type: 'number',
+            min: 0
+        },
+    },
+    paddingBottom: {
+        name: 'Padding Bottom',
+        type: {
+            name: 'string',
+            required: false
+        },
+        description: 'Description goes here...',
+        defaultValue: 30,
+        control: {
+            type: 'number',
+            min: 0
+        },
+    },
+    paddingLeft: {
+        name: 'Padding Left',
+        type: {
+            name: 'string',
+            required: false
+        },
+        description: 'Description goes here...',
+        defaultValue: 30,
+        control: {
+            type: 'number',
+            min: 0
+        },
+    },
+    border: {
+        name: 'Border',
+        type: {
+            name: 'string',
+            required: false
+        },
+        description: 'Description goes here...',
+        defaultValue: 1,
+        control: {
+            type: 'number',
+            min: 0
+        },
+    },
+    className: {
+        name: 'Extra Class(es)',
+        type: {
+            name: 'string',
+            required: false
+        },
+        description: 'Description goes here...',
+        control: {
+            type: 'text'
+        }
+    },
     children: {
         defaultValue: <Button label="Save Changes" color="blue" />
     }
