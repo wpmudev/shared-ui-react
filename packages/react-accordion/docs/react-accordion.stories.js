acd8f5e6d509918c9bf2b18771573c3403698788
veljkocukic <veljkocukic@gmail.com>
2021-05-12 08:43:37 +0200

    Replaced double-quotes for single-quotes

--- packages/react-accordion/docs/react-accordion.stories.js
+++ packages/react-accordion/docs/react-accordion.stories.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { Box, BoxBody } from "@wpmudev/react-box";
-import { Accordion, AccordionItem } from "../lib/react-accordion";
+import React from 'react';
+import { Box, BoxBody } from '@wpmudev/react-box';
+import { Accordion, AccordionItem } from '../lib/react-accordion';
 
 export default {
-  title: "Containers/Accordion",
+  title: 'Containers/Accordion',
 };
 
 const Template = ({ children, ...props }) => {
@@ -19,57 +19,57 @@ const Template = ({ children, ...props }) => {
 };
 
 export const primary = Template.bind({});
-primary.storyName = "Default";
+primary.storyName = 'Default';
 primary.args = {
   children: [
     {
-      color: "sui-error",
+      color: 'sui-error',
       header: [
         {
           title: true,
-          size: "4",
+          size: '4',
           content: (
             <>
               <span
-                aria-hidden="true"
-                className="sui-icon-warning-alert sui-error"
+                aria-hidden='true'
+                className='sui-icon-warning-alert sui-error'
               ></span>
               Item
             </>
           ),
         },
         {
-          size: "4",
+          size: '4',
           content: (
-            <div className="sui-circle-score sui-grade-f loaded" data-score="8">
-              <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                <circle stroke-width="16" cx="50" cy="50" r="42"></circle>
+            <div className='sui-circle-score sui-grade-f loaded' data-score='8'>
+              <svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+                <circle stroke-width='16' cx='50' cy='50' r='42'></circle>
                 <circle
-                  stroke-width="16"
-                  cx="50"
-                  cy="50"
-                  r="42"
-                  stroke-dasharray="0,2103.150263212341"
+                  stroke-width='16'
+                  cx='50'
+                  cy='50'
+                  r='42'
+                  stroke-dasharray='0,2103.150263212341'
                   style={{
-                    animation: "3s ease 0s 1 normal forwards running sui8",
+                    animation: '3s ease 0s 1 normal forwards running sui8',
                   }}
                 ></circle>
               </svg>
-              <span className="sui-circle-score-label">8</span>
+              <span className='sui-circle-score-label'>8</span>
             </div>
           ),
         },
         {
-          size: "2",
+          size: '2',
           content: (
             <>
               More information
-              <button className="sui-button-icon sui-button-icon sui-accordion-open-indicator">
+              <button className='sui-button-icon sui-button-icon sui-accordion-open-indicator'>
                 <span
-                  className="sui-icon-chevron-down"
-                  aria-hidden="true"
+                  className='sui-icon-chevron-down'
+                  aria-hidden='true'
                 ></span>
-                <span className="sui-screen-reader-text">Close section</span>
+                <span className='sui-screen-reader-text'>Close section</span>
               </button>
             </>
           ),
@@ -84,23 +84,23 @@ primary.args = {
       ),
     },
     {
-      color: "sui-success",
+      color: 'sui-success',
       header: [
         {
-          size: "auto",
+          size: 'auto',
           toggle: <h3>Item</h3>,
           content: <></>,
         },
         {
-          size: "auto",
+          size: 'auto',
           content: (
             <>
-              <button className="sui-button-icon sui-button-icon sui-accordion-open-indicator">
+              <button className='sui-button-icon sui-button-icon sui-accordion-open-indicator'>
                 <span
-                  className="sui-icon-chevron-down"
-                  aria-hidden="true"
+                  className='sui-icon-chevron-down'
+                  aria-hidden='true'
                 ></span>
-                <span className="sui-screen-reader-text">Close section</span>
+                <span className='sui-screen-reader-text'>Close section</span>
               </button>
             </>
           ),
@@ -115,7 +115,7 @@ primary.args = {
       ),
     },
     {
-      color: "sui-warning",
+      color: 'sui-warning',
       header: [
         {
           title: true,
@@ -123,7 +123,7 @@ primary.args = {
           content: (
             <>
               Title
-              <span className="sui-tag">Draft Title </span>
+              <span className='sui-tag'>Draft Title </span>
             </>
           ),
         },
@@ -131,7 +131,7 @@ primary.args = {
           size: 4,
           content: (
             <>
-              <div className="sui-accordion-item-date">
+              <div className='sui-accordion-item-date'>
                 <strong>Last Submission</strong> March 11, 2017 @ 10:18am
               </div>
             </>
@@ -141,56 +141,56 @@ primary.args = {
           size: 3,
           content: (
             <>
-              <button className="sui-button sui-button-ghost sui-accordion-item-action">
-                <span className="sui-icon-pencil" aria-hidden="true"></span>
+              <button className='sui-button sui-button-ghost sui-accordion-item-action'>
+                <span className='sui-icon-pencil' aria-hidden='true'></span>
                 Edit
               </button>
-              <div className="sui-dropdown sui-accordion-item-action">
-                <button className="sui-button-icon sui-dropdown-anchor">
+              <div className='sui-dropdown sui-accordion-item-action'>
+                <button className='sui-button-icon sui-dropdown-anchor'>
                   <span
-                    className="sui-icon-widget-settings-config"
-                    aria-hidden="true"
+                    className='sui-icon-widget-settings-config'
+                    aria-hidden='true'
                   ></span>
                 </button>
                 <ul>
                   <li>
-                    <a href="">
-                      <span className="sui-icon-eye" aria-hidden="true"></span>
+                    <a href=''>
+                      <span className='sui-icon-eye' aria-hidden='true'></span>
                       Preview
                     </a>
                   </li>
                   <li>
-                    <a href="">
+                    <a href=''>
                       <span
-                        className="sui-icon-community-people"
-                        aria-hidden="true"
+                        className='sui-icon-community-people'
+                        aria-hidden='true'
                       ></span>
                       View
                     </a>
                   </li>
                   <li>
-                    <a href="">
+                    <a href=''>
                       <span
-                        className="sui-icon-page-multiple"
-                        aria-hidden="true"
+                        className='sui-icon-page-multiple'
+                        aria-hidden='true'
                       ></span>
                       Duplicate
                     </a>
                   </li>
                   <li>
-                    <a href="">
+                    <a href=''>
                       <span
-                        className="sui-icon-cloud-migration"
-                        aria-hidden="true"
+                        className='sui-icon-cloud-migration'
+                        aria-hidden='true'
                       ></span>
                       Export
                     </a>
                   </li>
                   <li>
-                    <a href="">
+                    <a href=''>
                       <span
-                        className="sui-icon-trash"
-                        aria-hidden="true"
+                        className='sui-icon-trash'
+                        aria-hidden='true'
                       ></span>
                       Delete
                     </a>
@@ -198,12 +198,12 @@ primary.args = {
                 </ul>
               </div>
               <button
-                className="sui-button-icon sui-accordion-open-indicator"
-                aria-label="Open Item"
+                className='sui-button-icon sui-accordion-open-indicator'
+                aria-label='Open Item'
               >
                 <span
-                  className="sui-icon-chevron-down"
-                  aria-hidden="true"
+                  className='sui-icon-chevron-down'
+                  aria-hidden='true'
                 ></span>
               </button>
             </>
--- packages/react-accordion/lib/react-accordion.js
+++ packages/react-accordion/lib/react-accordion.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const Accordion = (props) => {
-  return <div className="sui-accordion">{props.children}</div>;
+  return <div className='sui-accordion'>{props.children}</div>;
 };
 
 const AccordionItem = (props) => {
@@ -9,22 +9,22 @@ const AccordionItem = (props) => {
   let [toggleOffOn, setToggleOffOn] = useState(false);
 
   function toggle(e) {
-    if ("sui-dropdown" !== e.target.className) {
+    if ('sui-dropdown' !== e.target.className) {
       setOpen(!open);
     }
   }
 
   const clazz = !open
-    ? "sui-accordion-item"
-    : "sui-accordion-item sui-accordion-item--open";
+    ? 'sui-accordion-item'
+    : 'sui-accordion-item sui-accordion-item--open';
 
-  const disabled = !toggleOffOn ? "" : "sui-accordion-item--disabled";
-  const color = props.color ? props.color : "";
+  const disabled = !toggleOffOn ? '' : 'sui-accordion-item--disabled';
+  const color = props.color ? props.color : '';
 
   return (
     <div className={`${clazz} ${disabled}  ${color}`} {...props}>
       <AccordionItemHeader
-        state={open ? "true" : "false"}
+        state={open ? 'true' : 'false'}
         header={props.header}
         setToggleOffOn={setToggleOffOn}
         onClick={(e) => toggle(e)}
@@ -39,29 +39,29 @@ const AccordionItemHeader = (props) => {
   let toggleBtn = (text) => {
     return (
       <div
-        className="sui-accordion-item-title sui-accordion-col-auto"
+        className='sui-accordion-item-title sui-accordion-col-auto'
         style={{
-          display: "flex",
-          minWidth: "6em",
-          justifyContent: "space-between",
-          alignItems: "center",
+          display: 'flex',
+          minWidth: '6em',
+          justifyContent: 'space-between',
+          alignItems: 'center',
         }}
       >
         <label
-          for="flexbox-accordion-sample-item-3"
-          className="sui-toggle sui-accordion-item-action"
+          for='flexbox-accordion-sample-item-3'
+          className='sui-toggle sui-accordion-item-action'
         >
           <input
-            type="checkbox"
-            id="flexbox-accordion-sample-item-3"
+            type='checkbox'
+            id='flexbox-accordion-sample-item-3'
             onClick={() =>
               props.setToggleOffOn((prev) => {
                 return !prev;
               })
             }
           />
-          <span aria-hidden="true" className="sui-toggle-slider"></span>
-          <span className="sui-screen-reader-text">Toggle</span>
+          <span aria-hidden='true' className='sui-toggle-slider'></span>
+          <span className='sui-screen-reader-text'>Toggle</span>
         </label>
         {text}
       </div>
@@ -70,10 +70,10 @@ const AccordionItemHeader = (props) => {
 
   let array = props.header.map((item, key) => {
     let size = !item.size
-      ? "sui-accordion-col-auto"
-      : "sui-accordion-col-" + item.size;
+      ? 'sui-accordion-col-auto'
+      : 'sui-accordion-col-' + item.size;
 
-    let title = item.title ? "sui-accordion-item-title" : "";
+    let title = item.title ? 'sui-accordion-item-title' : '';
 
     return (
       <div className={`${title} ${size}`} key={key}>
@@ -84,14 +84,14 @@ const AccordionItemHeader = (props) => {
   });
 
   return (
-    <div className="sui-accordion-item-header" {...props}>
+    <div className='sui-accordion-item-header' {...props}>
       {array}
     </div>
   );
 };
 
 const AccordionItemBody = (props) => {
-  return <div className="sui-accordion-item-body">{props.children}</div>;
+  return <div className='sui-accordion-item-body'>{props.children}</div>;
 };
 
 export { Accordion, AccordionItem, AccordionItemHeader, AccordionItemBody };
