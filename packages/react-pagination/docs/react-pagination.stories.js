9565662bcc635654ad130b4f942e447d46c815e9
Gowtham369 <ravipatigowtham369@gmail.com>
2021-06-05 23:08:24 +0530

    rendered elements in the bottom as per design

--- packages/react-pagination/docs/react-pagination.stories.js
+++ packages/react-pagination/docs/react-pagination.stories.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import { Pagination } from '../lib/react-pagination';
+import React from "react";
+import { Pagination } from "../lib/react-pagination";
 
 export default {
-	title: 'Components/Pagination',
+	title: "Components/Pagination",
 	component: Pagination,
-}
-
-/* export function childElements(){
-	return <><h1>Blahhhhhhhhhhh</h1>
-	{childrenArray.map((data,index)=><h5 key={index}>This is the sample data that I am working with. Id number is {data}</h5>)}
-	<h2>Footer</h2></>
-} */
-let childrenArray=new Array()
-for (let i = 1; i <= 24/* from here you can change the number of elements that can be inputed */; ++i)
-childrenArray.push(i)
-console.log(childrenArray)
-export const primary = ( args ) => <Pagination { ...args } ><h1>Blahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</h1>
-{childrenArray?.map((data,index)=><h5 key={index}>This is the sample data that I am working with. Id number is {data}</h5>)}
-<h2>Footer</h2></Pagination>;
-primary.storyName = 'Pagination';
-primary.args = {
-	limit:7,	//elements per page
-	results:true,	//number of results
-	skip:true,	//skip to last and first page buttons status
-	/* children:{
-	{childrenArray?.map((data,index)=><h5 key={index}>This is the sample data that I am working with. Id number is {data}</h5>)}
-	} */
 };
-primary.argTypes = {};
+
+let childrenArray = new Array();
+for (let i = 1; i <= 34 /* from here you can change the number of elements that can be inputed */; ++i)
+	childrenArray.push(<h5 key={i}>This is the sample data that I am working with. Id number is {i}</h5>);
+
+export const primary = args => <Pagination {...args} />;
+
+primary.storyName = "Pagination";
+primary.args = {
+	limit: 5, //elements per page
+	results: true, //number of results
+	skip: true, //skip to last and first page buttons status
+	children: childrenArray, //Array with child elements
+};
+primary.argTypes = {
+	limit: { type: "number" },
+	results: { type: "boolean" },
+	skip: { type: "boolean" },
+	children: { type: "array" },
+};
--- packages/react-pagination/lib/react-pagination.js
+++ packages/react-pagination/lib/react-pagination.js
@@ -1,110 +1,142 @@
-import React, { useState, useEffect } from 'react';
-export const Pagination = ({ limit, skip, results, ...props }) => {
-	
-	const childElements=props.children
-	const elements=React.Children.map(childElements, child => React.cloneElement(child)).length;
-	const [pages, setPages] = useState(elements/limit>parseInt(elements/limit) ? parseInt(elements/limit)+1 : elements/limit)
-	const [pagesArray, setPagesArray] = useState([])
-	const [selectedPage, setSelectedPage] = useState("1")
-	const [startIndex, setStartIndex] = useState(0)
-	const [endIndex, setEndIndex] = useState(pages>=5 ? 5 : pages)
-	const [pageClickCounter,setPageClickCounter] = useState(0)
-	const [elementsStartIndex,setElementsStartIndex]=useState(0)
-	const [elementsEndIndex,setElementsEndIndex]=useState(limit)
-	
+import React, { useState, useEffect } from "react";
+export const Pagination = ({ limit, skip, results, ...args }) => {
+	const childElements = args.children,
+		elements = childElements.length,
+		pages = elements / limit > parseInt(elements / limit) ? parseInt(elements / limit) + 1 : elements / limit,
+		[pagesArray, setPagesArray] = useState([]),
+		[selectedPage, setSelectedPage] = useState("1"),
+		[startIndex, setStartIndex] = useState(0),
+		[endIndex, setEndIndex] = useState(pages >= 5 ? 5 : pages),
+		[pageClickCounter, setPageClickCounter] = useState(0),
+		[elementsStartIndex, setElementsStartIndex] = useState(0),
+		[elementsEndIndex, setElementsEndIndex] = useState(limit);
+
 	useEffect(() => {
-		var pagesArray = []
-		for (let i = 1; i <= pages; ++i)
-			pagesArray.push(i)
-		setPagesArray(pagesArray)
-	}, [])
+		var pagesArray = [];
+		for (let i = 1; i <= pages; ++i) pagesArray.push(i);
+		setPagesArray(pagesArray);
+	}, []);
+
 	useEffect(() => {
-		(selectedPage>=endIndex) && incrementIndexes();
-		(selectedPage<=startIndex+1) && decrementIndexes()
-	}, [pageClickCounter])
+		selectedPage >= endIndex && incrementIndexes();
+		selectedPage <= startIndex + 1 && decrementIndexes();
+	}, [pageClickCounter]);
+
 	useEffect(() => {
-		console.log("elementsStartIndex,elementsEndIndex",elementsStartIndex,elementsEndIndex)
-		if(selectedPage!=="1"){
-			setElementsStartIndex(selectedPage*limit-limit)
-			setElementsEndIndex(selectedPage*limit)
+		console.log("elementsStartIndex,elementsEndIndex", elementsStartIndex, elementsEndIndex);
+		if (selectedPage !== "1") {
+			setElementsStartIndex(selectedPage * limit - limit);
+			setElementsEndIndex(selectedPage * limit);
 		}
-	}, [selectedPage])
+	}, [selectedPage]);
 
 	const handleSkipToFirstPage = () => {
-		setSelectedPage(1)
-		setStartIndex(0)
-		setEndIndex(pages>=5 ? 5 : pages)
-	}
+		setSelectedPage(1);
+		setStartIndex(0);
+		setEndIndex(pages >= 5 ? 5 : pages);
+	};
 
 	const handleSkipToLastPage = () => {
-		setSelectedPage(pages)
-		setStartIndex(pages>=5 ? pages-5 : 0)
-		setEndIndex(pages)
-	}
-	
+		setSelectedPage(pages);
+		setStartIndex(pages >= 5 ? pages - 5 : 0);
+		setEndIndex(pages);
+	};
+
 	const handlePreviousPage = () => {
-		((selectedPage > 1)) && setSelectedPage(selectedPage-1)
-		console.log("decrementIndexes",selectedPage-1<startIndex+1,"selectedPage",selectedPage)
-		decrementIndexes()
-	}
+		selectedPage > 1 && setSelectedPage(selectedPage - 1);
+		console.log("decrementIndexes", selectedPage - 1 < startIndex + 1, "selectedPage", selectedPage);
+		decrementIndexes();
+	};
 
 	const handleNextPage = () => {
-		(selectedPage < pages) && setSelectedPage(parseInt(selectedPage)+1)
-		console.log("incrementIndexes",selectedPage+1>endIndex,"selectedPage",selectedPage)
-		incrementIndexes()
-	}
+		selectedPage < pages && setSelectedPage(parseInt(selectedPage) + 1);
+		console.log("incrementIndexes", selectedPage + 1 > endIndex, "selectedPage", selectedPage);
+		incrementIndexes();
+	};
 
-	const decrementIndexes =()=>{
-		if((selectedPage-1<=startIndex+1) && (startIndex !== 0))
-		{
-			setStartIndex(startIndex - 1)
-			setEndIndex(endIndex - 1)
+	const decrementIndexes = () => {
+		if (selectedPage - 1 <= startIndex + 1 && startIndex !== 0) {
+			setStartIndex(startIndex - 1);
+			setEndIndex(endIndex - 1);
 		}
-	}
-	const incrementIndexes =()=>{
-		if((selectedPage+1>=endIndex) && (endIndex !== pages))
-		{
-			setStartIndex(startIndex + 1)
-			setEndIndex(endIndex + 1)
+	};
+	const incrementIndexes = () => {
+		if (selectedPage + 1 >= endIndex && endIndex !== pages) {
+			setStartIndex(startIndex + 1);
+			setEndIndex(endIndex + 1);
 		}
-	}
-	
-	const handlePreviousEllipsis = () =>{
-		setSelectedPage(startIndex>=5?endIndex-6:endIndex-startIndex-1)
-		setStartIndex(startIndex>=5?startIndex-5:0)
-		setEndIndex(startIndex>=5?endIndex-5:endIndex-startIndex)
-	}
-	const handleNextEllipsis = () =>{
-		setSelectedPage(pages-endIndex>=5?startIndex+7:pages-endIndex+startIndex+2)
-		setStartIndex(pages-endIndex>=5?startIndex+5:pages-endIndex+startIndex)
-		setEndIndex(pages-endIndex>=5?endIndex+5:pages)
-	}
+	};
 
-	const handlePageClick = async (page) => {
-		setSelectedPage(page)
-		setPageClickCounter(pageClickCounter+1)
-		console.log("Click",selectedPage)
-	}
-	console.log(startIndex, endIndex,"elements:",elements)
+	const handlePreviousEllipsis = () => {
+		setSelectedPage(startIndex >= 5 ? endIndex - 6 : endIndex - startIndex - 1);
+		setStartIndex(startIndex >= 5 ? startIndex - 5 : 0);
+		setEndIndex(startIndex >= 5 ? endIndex - 5 : endIndex - startIndex);
+	};
+	const handleNextEllipsis = () => {
+		setSelectedPage(pages - endIndex >= 5 ? startIndex + 7 : pages - endIndex + startIndex + 2);
+		setStartIndex(pages - endIndex >= 5 ? startIndex + 5 : pages - endIndex + startIndex);
+		setEndIndex(pages - endIndex >= 5 ? endIndex + 5 : pages);
+	};
+
+	const handlePageClick = async page => {
+		setSelectedPage(page);
+		setPageClickCounter(pageClickCounter + 1);
+		console.log("Click", selectedPage);
+	};
+	console.log(startIndex, endIndex, "elements:", elements);
 
 	return (
 		<>
-			{/* {childElements.slice(elementsStartIndex,elementsEndIndex)} */}
-			{ React.Children.map(childElements, child => React.cloneElement(child)).slice(elementsStartIndex,elementsEndIndex)}
+			{/* {React.Children.map(childElements, child => React.cloneElement(child)).slice(elementsStartIndex, elementsEndIndex)} */}
 			<div className="sui-pagination-wrap">
 				{results && <span className="sui-pagination-results">{elements} results</span>}
 				<ul className="sui-pagination">
-					{skip && <li onClick={handleSkipToFirstPage}><a disabled={selectedPage<=1} title="go to first page"><span className="sui-icon-arrow-skip-back" ></span></a></li>}
-					<li onClick={handlePreviousPage}><a disabled={selectedPage<=1}><span className="sui-icon-chevron-left" ></span></a></li>
-					{(startIndex>1) && <li onClick={handlePreviousEllipsis}><a>...</a></li>}
+					{skip && (
+						<li onClick={handleSkipToFirstPage}>
+							<a disabled={selectedPage <= 1} title="go to first page">
+								<span className="sui-icon-arrow-skip-back"></span>
+							</a>
+						</li>
+					)}
+					<li onClick={handlePreviousPage}>
+						<a disabled={selectedPage <= 1}>
+							<span className="sui-icon-chevron-left"></span>
+						</a>
+					</li>
+					{startIndex > 1 && (
+						<li onClick={handlePreviousEllipsis}>
+							<a>...</a>
+						</li>
+					)}
 					{pagesArray?.slice(startIndex, endIndex)?.map((data, index) => {
-							return <li onClick={()=>handlePageClick(parseInt(data))} key={index}><a className={selectedPage===data ? "sui-active" : ""}>{data}</a></li>
-						})}
-					{(endIndex<pages-1) && <li onClick={handleNextEllipsis}><a>...</a></li>}
-					<li onClick={handleNextPage}><a disabled={selectedPage>=pages}><span className="sui-icon-chevron-right"></span></a></li>
-					{skip && <li onClick={handleSkipToLastPage}><a disabled={selectedPage>=pages} title="go to last page"><span className="sui-icon-arrow-skip-forward"></span></a></li>}
+						return (
+							<li onClick={() => handlePageClick(parseInt(data))} key={index}>
+								<a aria-selected={selectedPage === data} className={selectedPage === data ? "sui-active" : ""}>
+									{data}
+								</a>
+							</li>
+						);
+					})}
+					{endIndex < pages - 1 && (
+						<li onClick={handleNextEllipsis}>
+							<a>...</a>
+						</li>
+					)}
+					<li onClick={handleNextPage}>
+						<a disabled={selectedPage >= pages}>
+							<span className="sui-icon-chevron-right"></span>
+						</a>
+					</li>
+					{skip && (
+						<li onClick={handleSkipToLastPage}>
+							<a disabled={selectedPage >= pages} title="go to last page">
+								<span className="sui-icon-arrow-skip-forward"></span>
+							</a>
+						</li>
+					)}
 				</ul>
 			</div>
+			{childElements.slice(elementsStartIndex, elementsEndIndex)}
 		</>
 	);
 };
