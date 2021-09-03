d086e58cbb7fc24c5da603c8feff37aaba9bff5d
Gowtham Ravipati <ravipatigowtham369@gmail.com>
2021-09-02 21:19:44 -0500

    React progress bar created with different placement accessibility

--- packages/react-progress-bar/lib/react-progress-bar.js
+++ packages/react-progress-bar/lib/react-progress-bar.js
@@ -1,7 +1,36 @@
-'use strict';
+import React, { useState, useEffect } from "react";
 
-module.exports = reactProgressBar;
+export const ProgressBar = ({ progress, ...args }) => {
+	return (
+		<div className={args.outlined && "sui-progress-block"}>
+			<div className="sui-progress">{args.content(progress)}</div>
+		</div>
+	);
+};
+export const bar = progress => {
+	return (
+		<div className="sui-progress-bar" aria-hidden="true">
+			{console.log("adasdf")}
+			<span style={{ width: progress + "%" }}></span>
+		</div>
+	);
+};
 
-function reactProgressBar() {
-    // TODO
-}
+export const pecentage = progress => {
+	return <span class="sui-progress-text">{progress}%</span>;
+};
+
+export const progressIcon = () => {
+	return (
+		<span class="sui-progress-icon" aria-hidden="true">
+			<span class="sui-icon-loader sui-loading"></span>
+		</span>
+	);
+};
+export const closeBtn = func => {
+	return (
+		<button onClick={func} class="sui-button-icon sui-tooltip" data-tooltip="Cancel">
+			<span class="sui-icon-close" aria-hidden="true"></span>
+		</button>
+	);
+};
