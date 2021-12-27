39c6271710f0ae1ade6fc7fc977e8a2d11cd2956
Pawan Kumar <pwnkumar201@gmail.com>
2021-12-27 22:37:57 +0530

    added react snippets

--- packages/react-snippets/lib/react-snippets.js
+++ packages/react-snippets/lib/react-snippets.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { useCopyToClipboard } from "@wordpress/compose";
+
+const CopyToClipboard = htmlCode => {
+	const [hasCopied, setHasCopied] = useState(false);
+	const CopyToClipboardRef = useCopyToClipboard(htmlCode, () => {
+		setHasCopied(true);
+	});
+
+	return (
+		<button
+			className={hasCopied ? "sui-button sui-tooltip" : "sui-button"}
+			aria-label={hasCopied ? "Copied!" : ""}
+			data-tooltip={hasCopied ? "Copied!" : ""}
+			onMouseLeave={() => setHasCopied(false)}
+			ref={CopyToClipboardRef}
+			data-clipboard-target="">
+			Copy
+		</button>
+	);
+};
 
 const Snippets = ({ copy, content }) => {
 	const htmlCode = content;
 
-	if (copy) {}
-
 	return (
 		<div className="sui-code-snippet-wrapper">
 			<pre className="sui-code-snippet">{htmlCode}</pre>
-			{copy && (
-				<button className="sui-button" data-clipboard-target>
-					Copy
-				</button>
-			)}
+			{copy && CopyToClipboard(htmlCode)}
 		</div>
 	);
 };
