846ee0065853f4901b5c07a0a1581e36bf15883f
Pawan Kumar <pwnkumar201@gmail.com>
2022-01-21 10:46:29 +0530

    react field list

--- packages/react-field-list/docs/react-field-list.stories.js
+++ packages/react-field-list/docs/react-field-list.stories.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import { Box, BoxBody } from '@wpmudev/react-box';
 import { FieldList } from '../lib/react-field-list';
 
 export default {
 	title: 'Components/Field list',
 	component: FieldList,
-    args: {
-        
-    }
+	args: {
+		title: 'Link To',
+		description: 'Choose content you want to convert to links.',
+	},
+	argTypes: {
+		title: {
+			description: 'Title of the field list',
+			control: {
+				type: 'text',
+			},
+		},
+		description: {
+			description: 'Description of the field list',
+			control: {
+				type: 'text',
+			},
+		},
+	},
 };
 
-const Template = (args) => <FieldList {...args} />;
+const Template = (args) => (
+	<Box>
+		<BoxBody>
+			<FieldList {...args} />
+		</BoxBody>
+	</Box>
+);
 
 export const primary = Template.bind({});
-primary.storyName = 'primary';
+primary.storyName = 'Default';
--- packages/react-field-list/lib/react-field-list.js
+++ packages/react-field-list/lib/react-field-list.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
-export default function FieldList({ text }) {
-	return <div>{text}</div>;
-}
+export const FieldList = ({ title, description }) => {
+	return (
+		<div className="sui-field-list">
+			<div className="sui-field-list-header">
+				<h3 className="sui-field-list-title">{title}</h3>
+
+				<p className="sui-description">{description}</p>
+			</div>
+
+			<div className="sui-field-list-body">
+				<div className="sui-field-list-item">
+					<label className="sui-field-list-item-label" htmlFor="toggle-1">
+						Posts
+					</label>
+
+					<label className="sui-toggle">
+						<input type="checkbox" id="toggle-1" />
+						<span className="sui-toggle-slider"></span>
+					</label>
+				</div>
+			</div>
+		</div>
+	);
+};
