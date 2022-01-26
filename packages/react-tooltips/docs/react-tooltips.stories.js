465d633ec96defd921198ec1188cea77f991d951
Pawan Kumar <pwnkumar201@gmail.com>
2022-01-26 23:25:14 +0530

    reduce css

--- packages/react-tooltips/docs/react-tooltips.stories.js
+++ packages/react-tooltips/docs/react-tooltips.stories.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Box, BoxBody } from '@wpmudev/react-box';
 import ToolTips from '../lib/react-tooltips';
 
 export default {
-	title: 'Components/Tooltips',
+	title: 'Components/Tooltips/Default',
 	component: ToolTips,
 	args: {
 		buttonText: 'Element content',
 		tooltipText: 'Example tooltip content goes here',
 		position: 'top',
 		id: 'tooltip-id',
+		customWidth: '171',
+		customMobileWidth: '200',
+		icon: false,
 	},
 	argTypes: {
 		buttonText: {
@@ -45,18 +49,89 @@ export default {
 				type: 'text',
 			},
 		},
+		customWidth: {
+			description: 'This will be the width of the tooltip',
+			control: {
+				type: 'text',
+			},
+		},
+		customMobileWidth: {
+			description: 'This will be the width of the tooltip in mobile view.',
+			control: {
+				type: 'text',
+			},
+		},
+		icon: {
+			description: 'This will be the icon of the tooltip',
+		},
 	},
 };
 
 const Template = (args) => {
-	return <ToolTips {...args} />;
+	return (
+		<Box>
+			<BoxBody>
+				<ToolTips {...args} />
+			</BoxBody>
+		</Box>
+	);
 };
 
 export const primary = Template.bind({});
 primary.storyName = 'Default';
 
+// top left position
 export const topLeft = Template.bind({});
 topLeft.storyName = 'Top Left';
 topLeft.args = {
 	position: 'top-left',
+};
+
+// top right position
+export const topRight = Template.bind({});
+topRight.storyName = 'Top Right';
+topRight.args = {
+	position: 'top-right',
+};
+
+// bottom position
+export const bottom = Template.bind({});
+bottom.storyName = 'Bottom';
+bottom.args = {
+	position: 'bottom',
+};
+
+// bottom left position
+export const bottomLeft = Template.bind({});
+bottomLeft.storyName = 'Bottom Left';
+bottomLeft.args = {
+	position: 'bottom-left',
+};
+
+// bottom right position
+export const bottomRight = Template.bind({});
+bottomRight.storyName = 'Bottom Right';
+bottomRight.args = {
+	position: 'bottom-right',
+};
+
+// left position
+export const left = Template.bind({});
+left.storyName = 'Left';
+left.args = {
+	position: 'left',
+};
+
+// right position
+export const right = Template.bind({});
+right.storyName = 'Right';
+right.args = {
+	position: 'right',
+};
+
+// icon button
+export const icon = Template.bind({});
+icon.storyName = 'Icon Buttton';
+icon.args = {
+	icon: true,
 };
--- packages/react-tooltips/lib/react-tooltips.js
+++ packages/react-tooltips/lib/react-tooltips.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import styled from 'styled-components';
 import { Button } from '@wpmudev/react-button';
+import { ButtonIcon } from '@wpmudev/react-button-icon';
 
 const Wrapper = styled.span`
 	position: relative;
 	display: inline-block;
 
-	&.sui-tooltip:after,
-	&.sui-tooltip:before {
-		content: unset !important;
+	&.sui-tooltip {
+		&:after,
+		&:before {
+			content: unset;
+		}
 	}
 
 	[role='tooltip'] {
+		min-width: var(--tooltip-width, 40px);
 		opacity: 0;
 		position: absolute;
 		left: 50%;
 		bottom: 100%;
-		margin-bottom: 7px;
+		margin-bottom: 10px;
 		padding: 8px 12px;
 		border-radius: 4px;
 		background-color: #333;
-		box-sizing: border-box;
 		transform: translateX(-50%);
 		color: #fff;
 		font: 500 12px/18px Roboto, Arial, sans-serif;
@@ -28,9 +31,16 @@ const Wrapper = styled.span`
 		text-transform: none;
 		white-space: nowrap;
 		transition: 0.2s;
+		@media (max-width: 782px) {
+			&.sui-tooltip-mobile {
+				min-width: var(--tooltip-width-mobile, 120px);
+				white-space: normal;
+			}
+		}
 	}
 
 	.sui__tooltip-elem {
+		position: unset;
 		margin-right: 0 !important;
 		&:after {
 			content: ' ';
@@ -39,7 +49,6 @@ const Wrapper = styled.span`
 			pointer-events: none;
 			position: absolute;
 			z-index: 9990;
-			-webkit-transition: 0.2s;
 			transition: 0.2s;
 			left: 50%;
 			bottom: 100%;
@@ -71,6 +80,15 @@ const Wrapper = styled.span`
 		transform: translateX(0);
 	}
 
+	&[class*='sui-tooltip-bottom-'] .sui__tooltip-elem::after,
+	&.sui-tooltip-bottom .sui__tooltip-elem::after {
+		top: 100%;
+		bottom: unset;
+		border-top-color: transparent;
+		border-bottom-color: #333;
+	}
+
+	&[class*='sui-tooltip-bottom-'] [role='tooltip'],
 	&.sui-tooltip-bottom [role='tooltip'] {
 		top: 100%;
 		bottom: unset;
@@ -79,8 +97,8 @@ const Wrapper = styled.span`
 	}
 
 	&.sui-tooltip-bottom-left [role='tooltip'] {
-		left: unset;
-		right: 0;
+		left: 0;
+		right: unset;
 		transform: translateX(0);
 	}
 
@@ -89,11 +107,64 @@ const Wrapper = styled.span`
 		right: 0;
 		transform: translateX(0);
 	}
+
+	&.sui-tooltip-right [role='tooltip'] {
+		top: 50%;
+		bottom: unset;
+		left: 100%;
+		margin-left: 10px;
+		margin-bottom: 0;
+		transform: translateY(-50%);
+	}
+
+	&.sui-tooltip-right .sui__tooltip-elem::after {
+		top: 50%;
+		bottom: unset;
+		left: 100%;
+		border-top-color: transparent;
+		border-right-color: #333;
+		transform: translateY(-50%);
+	}
+
+	&.sui-tooltip-left [role='tooltip'] {
+		top: 50%;
+		right: 100%;
+		bottom: unset;
+		left: unset;
+		margin-right: 10px;
+		margin-bottom: 0;
+		transform: translateY(-50%);
+	}
+
+	&.sui-tooltip-left .sui__tooltip-elem::after {
+		top: 50%;
+		right: 100%;
+		bottom: unset;
+		left: unset;
+		border-top-color: transparent;
+		border-left-color: #333;
+		transform: translateY(-50%);
+	}
 `;
 
-export default function ToolTips({ buttonText, tooltipText, position, id }) {
+export default function ToolTips({
+	buttonText,
+	tooltipText,
+	position,
+	id,
+	customWidth,
+	icon,
+	customMobileWidth,
+}) {
 	const [isOpen, setIsOpen] = useState(false);
+	let tooltipWidth = {};
 	let classes = isOpen ? 'sui-tooltip show ' : 'sui-tooltip ';
+
+	// if custom width is set, use it
+	customWidth ? (tooltipWidth['--tooltip-width'] = `${customWidth}px`) : '';
+
+	// if custom mobile width is set, use it
+	customMobileWidth ? (tooltipWidth['--tooltip-width-mobile'] = `${customMobileWidth}px`) : '';
 
 	// when escape key is pressed close the tooltip
 	const escFunction = useCallback(
@@ -129,23 +200,42 @@ export default function ToolTips({ buttonText, tooltipText, position, id }) {
 			break;
 	}
 
+	// if mobile width is set, add class
+	if (customMobileWidth) {
+		classes = classes + ' sui-tooltip-mobile';
+	}
+
 	return (
 		<Wrapper className={classes}>
-			<Button
-				label={buttonText}
-				className="sui__tooltip-elem"
-				aria-describedby={id}
-				onFocus={() => setIsOpen(true)}
-				onBlur={() => setIsOpen(false)}
-				onMouseEnter={() => setIsOpen(true)}
-				onMouseLeave={() => setIsOpen(false)}
-			/>
+			{icon ? (
+				<ButtonIcon
+					label="Close"
+					icon="info"
+					className="sui__tooltip-elem"
+					aria-describedby={id}
+					onFocus={() => setIsOpen(true)}
+					onBlur={() => setIsOpen(false)}
+					onMouseEnter={() => setIsOpen(true)}
+					onMouseLeave={() => setIsOpen(false)}
+				/>
+			) : (
+				<Button
+					label={buttonText}
+					className="sui__tooltip-elem"
+					aria-describedby={id}
+					onFocus={() => setIsOpen(true)}
+					onBlur={() => setIsOpen(false)}
+					onMouseEnter={() => setIsOpen(true)}
+					onMouseLeave={() => setIsOpen(false)}
+				/>
+			)}
 			<div
 				id={id}
 				role="tooltip"
 				aria-hidden={!isOpen}
 				aria-live="polite"
 				tab-index={isOpen ? '0' : '-1'}
+				style={tooltipWidth}
 			>
 				{tooltipText}
 			</div>
