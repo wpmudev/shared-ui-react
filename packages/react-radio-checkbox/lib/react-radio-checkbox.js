b876296a2275bd8c017cbb1efba5761fd786e759
Pawan Kumar <pwnkumar201@gmail.com>
2022-02-18 10:53:44 +0530

    added aria role

--- packages/react-radio-checkbox/lib/react-radio-checkbox.js
+++ packages/react-radio-checkbox/lib/react-radio-checkbox.js
@@ -13,7 +13,7 @@ export const RadioAndCheckbox = ({ size, stacked, type, options }) => {
     }
 
     return (
-        <div className="sui-form-field" role="radiogroup">
+        <div className="sui-form-field" role={type == 'radio' ? 'radiogroup' : null}>
             {options.map((option, index) => {
                 return (
                     option.image ?
@@ -25,14 +25,28 @@ export const RadioAndCheckbox = ({ size, stacked, type, options }) => {
                                 alt={option.image.alt}
                             />
                             <span className={mainClass.join(' ')}>
-                                <input id={option.id} type="radio" name={option.name} aria-labelledby={option.arialabelledby} defaultChecked={option.defaultChecked} disabled={option.disabled} />
+                                <input 
+                                    id={option.id} 
+                                    type={type} 
+                                    name={option.name} 
+                                    aria-labelledby={option.arialabelledby} 
+                                    defaultChecked={option.defaultChecked} 
+                                    disabled={option.disabled} 
+                                />
                                 <span aria-hidden="true"></span>
                                 <span id={option.arialabelledby}>{option.label}</span>
                             </span>
                         </label>
                     ) : (
                         <label className={mainClass.join(' ')} htmlFor={option.id} key={index}>
-                            <input id={option.id} type="radio" name={option.name} aria-labelledby={option.arialabelledby} defaultChecked={option.defaultChecked} disabled={option.disabled} />
+                            <input 
+                                id={option.id} 
+                                type={type} 
+                                name={option.name} 
+                                aria-labelledby={option.arialabelledby} 
+                                defaultChecked={option.defaultChecked} 
+                                disabled={option.disabled} 
+                            />
                             <span aria-hidden="true"></span>
                             <span id={option.arialabelledby}>{option.label}</span>
                         </label>
