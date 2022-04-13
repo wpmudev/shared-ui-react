8950cea4ad0e45d09583beb60c2156036ecf1e76
Pawan Kumar <pwnkumar201@gmail.com>
2022-04-13 16:11:53 +0530

    adding docs for input component

---
+++ packages/react-input/docs/input-modifiers.stories.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Input } from '../lib/react-input';
+
+export default {
+	title: 'Components/Input',
+	component: Input,
+};
+
+export const demo = (args) => <Input {...args} />;
+demo.storyName = 'Demo';
+demo.args = {
+	label: 'Company Name',
+	placeholder: 'Eg. Acme Industries',
+	description: 'Maecenas sed diam eget risus varius blandit sit amet non magna.',
+	errorStatus: false,
+	errorDescription: 'Here is a description of the error',
+	size: 'large',
+};
+demo.argTypes = {
+	label: {
+		description: 'The label of the input',
+		table: {
+			type: { summary: 'string' },
+			defaultValue: { summary: '' },
+		},
+		control: {
+			type: 'text',
+		},
+	},
+	maxLength: {
+		description: 'The maximum length of the input',
+		table: {
+			type: { summary: 'number' },
+			defaultValue: { summary: '' },
+		},
+		control: {
+			type: 'number',
+		},
+	},
+	placeholder: {
+		description: 'The placeholder of the input',
+		table: {
+			type: { summary: 'string' },
+			defaultValue: { summary: '' },
+		},
+		control: {
+			type: 'text',
+		},
+	},
+	description: {
+		description: 'The description of the input',
+		table: {
+			type: { summary: 'string' },
+			defaultValue: { summary: '' },
+		},
+		control: {
+			type: 'text',
+		},
+	},
+	errorStatus: {
+		description: 'Whether the input has an error',
+		table: {
+			type: { summary: 'boolean' },
+			defaultValue: { summary: false },
+		},
+		control: {
+			type: 'boolean',
+		},
+	},
+	errorDescription: {
+		description: 'The error description of the input',
+		table: {
+			type: { summary: 'string' },
+			defaultValue: { summary: '' },
+		},
+		control: {
+			type: 'text',
+		},
+	},
+	size: {
+		description: 'The size of the input',
+		table: {
+			type: { summary: 'string' },
+			defaultValue: { summary: 'large' },
+		},
+		control: {
+			type: 'select',
+			options: ['large', 'small', 'medium'],
+		},
+	},
+	type: {
+		description: 'The type of the input',
+		table: {
+			type: { summary: 'string' },
+			defaultValue: { summary: 'text' },
+		},
+		control: {
+			type: 'text',
+		},
+	},
+};
---
+++ packages/react-input/docs/input-states.stories.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import { Input } from '../lib/react-input';
+
+export default {
+	title: 'Components/Input/States',
+	component: Input,
+	parameters: {
+		actions: {
+			disabled: true,
+		},
+		controls: {
+			disabled: true,
+		},
+		notes: {
+			disabled: true,
+		},
+		previewTabs: {
+			'storybook/docs/panel': {
+				hidden: true,
+			},
+		},
+	},
+};
+
+const Template = ({ children }) => {
+	return (
+		<div className="sui-box">
+			<div className="sui-box-body">
+				{children}
+			</div>
+		</div>
+	);
+};
+
+const Title = ({ children }) => {
+	const customStyles = {
+		margin: 0,
+		marginBottom: 10 + 'px',
+		color: '#333',
+		fontSize: 14 + 'px',
+		lineHeight: 22 + 'px',
+	};
+
+	return (
+		<h3 style={ customStyles }>{ children }</h3>
+	);
+}
+
+const Description = ({ children }) => {
+	const customStyles = {
+		margin: 0,
+		marginBottom: 10 + 'px',
+	};
+
+	return (
+		<p className="sui-description" style={ customStyles }>
+			{ children }
+		</p>
+	);
+}
+
+const Code = ({ spaceTop = 5, spaceBottom = 20, children }) => {
+	const customStyles = {
+		display: 'block',
+		margin: 0,
+		marginTop: spaceTop + 'px',
+		marginBottom: spaceBottom + 'px',
+		padding: 5 + 'px ' + 10 + 'px',
+	};
+
+	return (
+		<code style={ customStyles }>{ children }</code>
+	);
+}
+
+const Section = ({ title, description, code, code2, isDefault = false, isLast = false, children }) => {
+	return (
+		<>
+			{ title && '' !== title && (
+				<Title>{ title }{ isDefault && (
+					<span
+						className="sui-tag sui-tag-sm sui-tag-yellow"
+						style={ { marginLeft: 10 + 'px' } }
+						aria-hidden="true"
+					>Default</span>
+				)}</Title>
+			)}
+			{ description && '' !== description && (
+				<Description>{ description }</Description>
+			)}
+			{ code && '' !== code && (
+				<Code { ... ( code2 && '' !== code2 ) && { spaceBottom: 5 } }>{ code }</Code>
+			)}
+			{ code2 && '' !== code2 && (
+				<Code>{ code2 }</Code>
+			)}
+			{ children }
+			{ !isLast && <hr /> }
+		</>
+	);
+}
+
+export const primary = () => {
+	return (
+		<Template>
+			<Section
+				title="Default Input"
+				description="This is the default state of a input."
+				code="&lt;Input description=&quot;Text description.&quot; label=&quot;Text Label&quot; placeholder=&quot;Placeholder&quot; type=&quot;text&quot; /&gt;"
+				isDefault={ true }
+				isLast={ true }>
+				<Input
+					description="Text description."
+					label="Text Label"
+					placeholder="Placeholder"
+					type="text"
+				/>
+			</Section>
+		</Template>
+	);
+};
+primary.storyName = 'Static';
+
+export const secondary = () => {
+	return (
+		<Template>
+			<Section
+				title="Error Input"
+				description="This is the default state of a input."
+				code="&lt;Input description=&quot;Text description.&quot; errorStatus={true} errorDescription=&quot;Error description.&quot; label=&quot;Text Label&quot; placeholder=&quot;Placeholder&quot; type=&quot;text&quot; /&gt;"
+				isDefault={ false }
+				isLast={ true }>
+				<Input
+					description="Text description."
+					errorStatus={true}
+					errorDescription="Error description."
+					label="Text Label"
+					placeholder="Placeholder"
+					type="text"
+				/>
+			</Section>
+		</Template>
+	);
+};
+secondary.storyName = 'Error';
