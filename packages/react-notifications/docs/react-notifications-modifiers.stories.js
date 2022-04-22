4972712568c6133dad7903ab6fdda135b2b54619
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2022-04-22 13:51:41 -0500

    📝 docs(notifications): Remove unnecessary function.

--- packages/react-notifications/docs/react-notifications-modifiers.stories.js
+++ packages/react-notifications/docs/react-notifications-modifiers.stories.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Notifications } from '../lib/react-notifications';
 
-const func = () => {
-	console.log('You called an callback function...');
-};
-
 export default {
 	title: 'Components/Notifications/Modifiers',
 	component: Notifications,
