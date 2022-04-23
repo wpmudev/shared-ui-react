63d4f70d73efdf208f07ac985cc1b3a718691c86
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2022-04-23 00:48:58 -0500

    🔥 fix(input): Remove new features from this PR.

--- packages/react-input/lib/react-input.js
+++ packages/react-input/lib/react-input.js
@@ -2,46 +2,40 @@ import React from 'react';
 
 const Input = ({
 	id,
-	constrainedField,
 	size,
 	label,
 	description,
 	type = 'text',
 	errorStatus,
 	errorDescription,
-	prefix,
-	suffix,
 	...props
 }) => {
 	const uniqueId = id && '' !== id ? id : props.property;
 
-	let clazz = 'sui-form-control' + (suffix ? ' sui-field-has-suffix' : '') + (prefix ? ' sui-field-has-prefix' : '');
-	let sizeClass = '';
+	let clazz = 'sui-form-control';
 
 	switch (size) {
 		case 'sm':
 		case 'small':
-			sizeClass += ' sui-input-sm';
+			clazz += ' sui-input-sm';
 			break;
 
 		case 'md':
 		case 'medium':
-			sizeClass += ' sui-input-md';
+			clazz += ' sui-input-md';
 			break;
 	}
 
 	return (
-		<div className={`sui-form-field${errorStatus ? ' sui-form-field-error' : ''}${constrainedField ? sizeClass : ''}`}>
+		<div className={`sui-form-field${errorStatus ? ' sui-form-field-error' : ''}`}>
 			{label && (
 				<label htmlFor={uniqueId} className="sui-label">
 					{label}
 				</label>
 			)}
-			
-			{prefix && (<span className="sui-field-prefix">{prefix}</span>)}
-			<input id={uniqueId} type={type} className={`${!constrainedField ? clazz + sizeClass : clazz}`} {...props} />
-			{suffix && (<span className="sui-field-suffix">{suffix}</span>)}
-			
+
+			<input id={uniqueId} type={type} className={clazz} {...props} />
+
 			{errorStatus && errorDescription && (
 				<div className="sui-error-message">{errorDescription}</div>
 			)}
