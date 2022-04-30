a4661c254b9843c46b1937292c84075a352abf21
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2022-04-30 18:55:15 -0500

    📝 Move “Roadmap” page content to `docs` tab.

--- packages/core/docs/roadmap.stories.js
+++ packages/core/docs/roadmap.stories.js
@@ -1,59 +1,18 @@
-import React from 'react';
-import { WelcomePage, Article, Section } from './components';
+import CustomDocs from './roadmap.mdx';
 
 export default {
 	title: 'Roadmap',
 	parameters: {
-		notes: {
-			disable: true,
-		},
 		previewTabs: {
-			'storybook/docs/panel': {
+			canvas: {
 				hidden: true,
 			},
 		},
+		docs: {
+			page: CustomDocs,
+		},
+		viewMode: 'docs',
 	},
 };
 
-export const Roadmap = () => {
-	return (
-		<WelcomePage>
-			<Article
-				heading="Roadmap"
-				subheading="This is the roadmap of SUI React, based on our planned design and development path. Subject to change based on user needs and feedback."
-			>
-				<Section title="Q1 2022">
-					<ul style={ { marginBottom: 0 } }>
-						<li>Plan 1</li>
-						<li>Plan 2</li>
-						<li>Plan 3</li>
-					</ul>
-				</Section>
-
-				<Section title="Q2 2022">
-					<ul style={ { marginBottom: 0 } }>
-						<li>Plan 1</li>
-						<li>Plan 2</li>
-						<li>Plan 3</li>
-					</ul>
-				</Section>
-
-				<Section title="Q3 2022">
-					<ul style={ { marginBottom: 0 } }>
-						<li>Plan 1</li>
-						<li>Plan 2</li>
-						<li>Plan 3</li>
-					</ul>
-				</Section>
-
-				<Section title="Q4 2022">
-					<ul style={ { marginBottom: 0 } }>
-						<li>Plan 1</li>
-						<li>Plan 2</li>
-						<li>Plan 3</li>
-					</ul>
-				</Section>
-			</Article>
-		</WelcomePage>
-	);
-};
+export const Roadmap = () => {};
