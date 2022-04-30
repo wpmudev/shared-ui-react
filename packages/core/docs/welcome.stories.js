1a6663ec473f1c80c42e0cc148f79f82a38c35d7
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2022-04-30 18:54:56 -0500

    📝 Move “Welcome” page content to `docs` tab.

--- packages/core/docs/welcome.stories.js
+++ packages/core/docs/welcome.stories.js
@@ -1,46 +1,18 @@
-import React from 'react';
-import { WelcomePage, WelcomeBanner } from './components';
+import CustomDocs from './welcome.mdx';
 
 export default {
 	title: 'Welcome',
 	parameters: {
-		notes: {
-			disable: true,
-		},
 		previewTabs: {
-			'storybook/docs/panel': {
+			canvas: {
 				hidden: true,
 			},
 		},
+		docs: {
+			page: CustomDocs,
+		},
+		viewMode: 'docs',
 	},
 };
 
-export const Welcome = () => {
-	return (
-		<WelcomePage>
-			<WelcomeBanner
-				heading="SUI React."
-				subheading={
-					[
-						'Find ',
-						<span>documentation</span>,
-						' and ',
-						<span>resources</span>,
-						' for the React components of ',
-						<a href="https://wpmudev.com/" target="blank" rel="nofollow">WPMU DEV's</a>,
-						' design system to build WordPress plugins using our ',
-						<a
-							href="https://wpmudev.github.io/shared-ui/"
-							target="blank"
-							rel="nofollow">
-							Shared UI Library
-						</a>,
-						'.'
-					]
-				}
-				ctaLabel="Get started"
-				ctaLink="Getting Started"
-			/>
-		</WelcomePage>
-	);
-};
+export const Welcome = () => {};
