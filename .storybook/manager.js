5100029157762937fe9c157f18a95ac9a5c2d5dd
Pawan Kumar <pwnkumar201@gmail.com>
2022-05-03 19:12:28 +0530

    ✨ enhance(global): modified default theme of storybook.

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,9 +1,15 @@
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming/create';
+import Logo from '../assets/logo.svg';
+import '../assets/global.css';
 
 const customTheme = create({
 	base: 'light',
 	brandTitle: 'Shared UI React',
+	brandUrl: 'https://wpmudev.github.io/shared-ui-react/',
+  	brandImage: Logo,
+	appBg: '#091A2F',
+	fontBase: 'Roboto,Arial,sans-serif',
 });
 
 addons.setConfig({
