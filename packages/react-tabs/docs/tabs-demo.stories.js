1804ffd3336d741dd98a1aea9420b63ce95ee063
Pawan Kumar <pwnkumar201@gmail.com>
2022-06-06 11:09:58 +0530

    ✨ new(tabs): added flushed and overflow tabs.

--- packages/react-tabs/docs/tabs-demo.stories.js
+++ packages/react-tabs/docs/tabs-demo.stories.js
@@ -32,7 +32,94 @@ demo.args = {
             title: 'Tab 3',
             content_id: 'tab-3-content',
             content: <p>Content. Tab 3.</p>,
-        }
+        },
+        {
+            id: 'tab-4',
+            title: 'Tab 4',
+            content_id: 'tab-4-content',
+            content: <p>Content. Tab 4.</p>,
+        },
+        {
+            id: 'tab-5',
+            title: 'Tab 5',
+            content_id: 'tab-5-content',
+            content: <p>Content. Tab 5.</p>,
+        },
+        {
+            id: 'tab-6',
+            title: 'Tab 6',
+            content_id: 'tab-6-content',
+            content: <p>Content. Tab 6.</p>,
+        },
+        {
+            id: 'tab-7',
+            title: 'Tab 7',
+            content_id: 'tab-7-content',
+            content: <p>Content. Tab 7.</p>,
+        },
+        {
+            id: 'tab-8',
+            title: 'Tab 8',
+            content_id: 'tab-8-content',
+            content: <p>Content. Tab 8.</p>,
+        },
+        {
+            id: 'tab-9',
+            title: 'Tab 9',
+            content_id: 'tab-9-content',
+            content: <p>Content. Tab 9.</p>,
+        },
+        {
+            id: 'tab-10',
+            title: 'Tab 10',
+            content_id: 'tab-10-content',
+            content: <p>Content. Tab 10.</p>,
+        },
+        {
+            id: 'tab-11',
+            title: 'Tab 11',
+            content_id: 'tab-11-content',
+            content: <p>Content. Tab 11.</p>,
+        },
+        {
+            id: 'tab-12',
+            title: 'Tab 12',
+            content_id: 'tab-12-content',
+            content: <p>Content. Tab 12.</p>,
+        },
+        {
+            id: 'tab-13',
+            title: 'Tab 13',
+            content_id: 'tab-13-content',
+            content: <p>Content. Tab 13.</p>,
+        },
+        {
+            id: 'tab-14',
+            title: 'Tab 14',
+            content_id: 'tab-14-content',
+            content: <p>Content. Tab 14.</p>,
+        },
+        {
+            id: 'tab-15',
+            title: 'Tab 15',
+            content_id: 'tab-15-content',
+            content: <p>Content. Tab 15.</p>,
+        },
+        {
+            id: 'tab-16',
+            title: 'Tab 16',
+            content_id: 'tab-16-content',
+            content: <p>Content. Tab 16.</p>,
+        },
     ],
-    radio: true
+    radio: true,
+    type: 'default',
 };
+demo.argTypes = {
+    type: {
+        control: {
+            type: 'select',
+            options: ['default', 'flushed', 'overflow'],
+        },
+    }
+}
--- packages/react-tabs/lib/react-tabs.js
+++ packages/react-tabs/lib/react-tabs.js
@@ -1,14 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 const Tabs = ({
     tabs = '',
-    radio = false, 
+    radio = false,
+    type = ''
 }) => {
     const [activeTab, setActiveTab] = useState(0);
 
+    const tabRefs = useRef([]);
+
+    // For easy reference.
+    const keys = {
+        end: 35,
+        home: 36,
+        left: 37,
+        up: 38,
+        right: 39,
+        down: 40,
+        delete: 46,
+        enter: 13,
+        space: 32
+    };
+
+    // Add or substract depending on key pressed.
+    const direction = {
+        37: -1,
+        38: -1,
+        39: 1,
+        40: 1
+    };
+
+    let classes = 'sui-tabs';
+    
+    type === 'flushed' && (classes += ' sui-tabs-flushed');
+
+    type === 'overflow' && (classes += ' sui-tabs-overflow');
+
+    const scrollPosition = (direction, speed, distance, step) => {
+        const tabs = tabRefs.current;
+        let scrollAmount = 0;
+        var slideTimer = setInterval(function(){
+            direction === 'left' ?
+                tabs.scrollLeft -= step :
+                tabs.scrollLeft += step;
+            scrollAmount += step;
+            if(scrollAmount >= distance) {
+                clearTimeout(slideTimer);
+            }
+        }, speed);
+    }
+
     return (
-        <div className="sui-tabs">
-            <div role="tablist" className="sui-tabs-menu">
+        <div className={classes}>
+            {type === 'overflow' && (
+                <div tabIndex={-1} className="sui-tabs-navigation" aria-hidden="true">
+                    <button type="button" className="sui-button-icon sui-tabs-navigation--left" onClick={() => scrollPosition('left',25,100,10)}>
+                        <span className="sui-icon-chevron-left"></span>
+                    </button>
+                    <button type="button" className="sui-button-icon sui-tabs-navigation--right" onClick={() => scrollPosition('right',25,100,10)}>
+                        <span className="sui-icon-chevron-right"></span>
+                    </button>
+                </div>
+            )}
+
+            <div role="tablist" className="sui-tabs-menu" ref={tabRefs}>
                 {
                     tabs.map((tab, index) => (
                         <>
@@ -32,7 +87,7 @@ const Tabs = ({
                                         className="sui-screen-reader-text"
                                         aria-label={tab.title}
                                         aria-hidden="true"
-                                        checked={activeTab === index ? true : false}
+                                        defaultChecked={activeTab === index ? true : false}
                                     />
                             )}
                         </>
