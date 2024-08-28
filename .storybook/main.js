ca7acc4509cf0dd2fc038591fd1cf489e85a74d9
Pawan Kumar <pwnkumar201@gmail.com>
2024-08-28 09:58:00 +0530

    Added comment for main js.

--- .storybook/main.js
+++ .storybook/main.js
@@ -24,6 +24,7 @@ module.exports = {
 		// https://www.styled-components.com/docs/faqs#why-am-i-getting-a-warning-about-several-instances-of-module-on-the-page
 		config.resolve.alias['styled-components'] = path.resolve('node_modules', 'styled-components');
 
+		// Return the altered config
 		return config;
 	},
 
