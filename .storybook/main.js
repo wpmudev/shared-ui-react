f2e6b30ee3d120bb0b9c7569f8762049796070c2
Pawan Kumar <pwnkumar201@gmail.com>
2024-08-29 13:31:07 +0530

    💥 breaking: migrated yarn to npm.

--- .storybook/main.js
+++ .storybook/main.js
@@ -21,8 +21,6 @@ module.exports = {
 		// https://www.styled-components.com/docs/faqs#why-am-i-getting-a-warning-about-several-instances-of-module-on-the-page
 		config.resolve.alias['styled-components'] = path.resolve('node_modules', 'styled-components');
 
-		console.log(config.resolve);
-
 		// Return the altered config
 		return config;
 	},
